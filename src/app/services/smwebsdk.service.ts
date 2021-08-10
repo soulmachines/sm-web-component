10c5e450a0f00cf40e99f0e2a04211cae837345a
Yvonne Yang <yvonne.yang@soulmachines.com>
2021-08-10 17:27:29 +1200

    feat: Split relevant functionality out into Web SDK service QUIC-534

---
+++ src/app/services/smwebsdk.service.ts
@@ -0,0 +1,86 @@
+import { HttpClient } from '@angular/common/http';
+import { ElementRef, Injectable } from '@angular/core';
+import { Persona, Scene } from '@soulmachines/smwebsdk';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+import { SoulMachinesConfig } from '../video/soulmachines-config';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SMWebSDKService {
+  public smwebsdk: any;
+  public scene: Scene;
+  public persona: Persona;
+
+  private videoElement: HTMLVideoElement;
+  public personaVideoStream: MediaStream | MediaSource | Blob;
+
+  public connected = false;
+
+  constructor(private http: HttpClient) {
+    this.videoElement = document.createElement('video');
+    this.scene = new Scene(this.videoElement);
+    this.persona = new Persona(this.scene, 1);
+  }
+
+  public connect(tokenServer: string): Observable<string> {
+    if (!tokenServer) {
+      throw new Error('Unable to establish a connection, tokenServer missing');
+    }
+
+    const retryOptions = {
+      maxRetries: 20,
+      delayMs: 500,
+    };
+
+    this.scene.onStateEvent.addListener(this.onState);
+    this.scene.onDisconnectedEvent.addListener(this.onDisconnect);
+
+    return this.http.get<SoulMachinesConfig>(tokenServer).pipe(
+      switchMap((config: SoulMachinesConfig) =>
+        this.scene.connect(config.url, '', config.jwt, retryOptions),
+      ),
+      tap(() => {
+        this.connected = true;
+        this.personaVideoStream = this.videoElement.srcObject;
+      }),
+    );
+  }
+
+  public disconnect(): void {
+    if (this.scene) {
+      this.scene.disconnect();
+      this.scene = null;
+      // this does not actually guarantee that the ws connection
+      // has been closed. this is only that the close has been requested.
+      this.onDisconnect('userEnded');
+    }
+  }
+
+  public sendVideoBounds(width, height): void {
+    this.scene.sendVideoBounds(width, height);
+  }
+
+  public setMicrophoneEnabled(enabled: boolean) {
+    if (enabled) {
+      this.scene.startRecognize();
+    } else {
+      this.scene.stopRecognize();
+    }
+  }
+
+  private onDisconnect(reason: string) {
+    this.connected = false;
+
+    this.scene?.onStateEvent.removeListener(this.onState);
+    this.scene?.onDisconnectedEvent.removeListener(this.onDisconnect);
+  }
+
+  private onState(_: Scene, messageBody: any) {
+    if (messageBody.persona) {
+      const data = messageBody.persona[1];
+      console.log('STATE MESSAGE: ', data);
+    }
+  }
+}
--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -11,20 +11,21 @@ import {
   Output,
   EventEmitter,
 } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { SoulMachinesConfig } from './soulmachines-config';
 import { ResizeObserver } from '@juggle/resize-observer';
 import { Scene, Persona } from '@soulmachines/smwebsdk';
+import { SMWebSDKService } from '../services/smwebsdk.service';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.scss'],
+  // providers: [SMWebSDKService],
 })
 export class VideoComponent implements OnChanges, AfterViewInit, OnDestroy {
   private resizeObserver: ResizeObserver;
-  private persona: Persona;
-  private scene: Scene;
 
   // required inputs
   @Input() public tokenserver: string;
@@ -49,19 +50,32 @@ export class VideoComponent implements OnChanges, AfterViewInit, OnDestroy {
   // elements
   @ViewChild('video', { static: false }) videoRef: ElementRef;
 
-  constructor(private http: HttpClient, private hostRef: ElementRef) {
-    this.log('video: constructor', this.tokenserver);
+  constructor(private hostRef: ElementRef, private webSDKService: SMWebSDKService) {
+    this.log(`video constructor: token server - ${this.tokenserver}`);
 
-    // publicly accessible functions
-    this.hostRef.nativeElement.disconnect = () => this.disconnect();
+    //publicly accessible functions
+    this.hostRef.nativeElement.disconnect = () => this.webSDKService?.disconnect();
     this.hostRef.nativeElement.setMicrophoneEnabled = (value: boolean) =>
-      this.setMicrophoneEnabled(value);
+      this.webSDKService.setMicrophoneEnabled(value);
   }
 
   public ngOnChanges(changes: SimpleChanges) {
     this.log({ changes });
+    this.connect();
+  }
+
+  public connect() {
     if (this.tokenserver && this.autoconnect) {
-      this.connect();
+      this.webSDKService
+        .connect(this.tokenserver)
+        .pipe(
+          tap(() => this.onConnectionSuccess()),
+          catchError((e) => {
+            this.onConnectionError(e);
+            return of(false);
+          }),
+        )
+        .subscribe();
     }
   }
 
@@ -70,64 +84,22 @@ export class VideoComponent implements OnChanges, AfterViewInit, OnDestroy {
   }
 
   public ngOnDestroy() {
+    this.webSDKService.personaVideoStream = null;
     this.resizeObserver.unobserve(this.hostRef.nativeElement);
-    this.scene?.onStateEvent.removeListener(this.onState);
-    this.scene?.onDisconnectedEvent.removeListener(this.onDisconnected);
+    this.webSDKService.disconnect();
   }
 
-  public connect() {
-    this.http
-      .get<SoulMachinesConfig>(this.tokenserver)
-      .pipe(tap((config) => this.initialiseScene(config)))
-      .subscribe();
-  }
-
-  private async initialiseScene(config: SoulMachinesConfig): Promise<any> {
-    if (!this.videoRef) {
-      return;
-    }
-
-    this.scene = new Scene(this.videoRef.nativeElement as HTMLElement);
-    const retryOptions = {
-      maxRetries: 20,
-      delayMs: 500,
-    };
-    await this.scene.connect(config.url, '', config.jwt, retryOptions);
-    this.persona = new Persona(this.scene, 1);
-
-    //state messageBody type is StateResponseBody in smwebsdk, but not exposed publicly.
-    //listen and log the state message is for dev process, not sure if it should be exposed outside of sm.
-    this.scene.onStateEvent.addListener(this.onState);
-    this.scene.onDisconnectedEvent.addListener(this.onDisconnected);
-
+  private onConnectionSuccess() {
+    this.log(`session connected.`);
     this.resizeVideoStream();
   }
 
-  private onState(_: Scene, messageBody: any) {
-    if (messageBody.persona) {
-      const data = messageBody.persona[1];
-      console.log('STATE: ', data);
-    }
+  private onConnectionError(error: any) {
+    this.log(`session connection failed, error: ${error}`);
   }
 
   private onDisconnected(event: any) {
     this.disconnectEvent.emit(event.detail);
-  }
-
-  public disconnect() {
-    this.scene?.disconnect();
-  }
-
-  public isConnected(): boolean {
-    return Boolean(this.scene?.isConnected());
-  }
-
-  public setMicrophoneEnabled(enabled: boolean) {
-    if (enabled) {
-      this.scene.startRecognize();
-    } else {
-      this.scene.stopRecognize();
-    }
   }
 
   private initHostResizeWatcher() {
@@ -136,10 +108,10 @@ export class VideoComponent implements OnChanges, AfterViewInit, OnDestroy {
   }
 
   private resizeVideoStream() {
-    if (this.scene?.isConnected()) {
+    if (this.webSDKService?.connected) {
       const element = this.hostRef.nativeElement;
-      const [width, height] = [element.clientWidth, element.height];
-      this.scene.sendVideoBounds(width, height);
+      const [width, height] = [element.clientWidth, element.clientHeight];
+      this.webSDKService.sendVideoBounds(width, height);
     }
   }
 
