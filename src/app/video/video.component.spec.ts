59bef727cf4fc08175d532c9476939ee67296777
wade.fleming <wade.fleming@soulmachines.com>
2021-08-26 18:33:45 +1200

    test: Get existing tests running successfully QUIC-707

--- src/app/video/video.component.spec.ts
+++ src/app/video/video.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { VideoComponent } from './video.component';
 
 describe('VideoComponent', () => {
@@ -9,6 +9,7 @@ describe('VideoComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [VideoComponent],
+      imports: [HttpClientTestingModule],
     }).compileComponents();
   });
 
