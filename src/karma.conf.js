d0c7fd451f3f80b1b37c911d3cef45f03abaa3f9
wade.fleming <wade.fleming@soulmachines.com>
2021-08-27 16:31:44 +1200

    specify only ChromeHeadlessCustom

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadlessCustom'],
+    browsers: ['ChromeHeadlessCustom'],
     singleRun: false,
     restartOnFileChange: true,
     customLaunchers: {
