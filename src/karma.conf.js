07bfaece7e1f725cc3f7f3918eb16ff2d84d96ae
wade.fleming <wade.fleming@soulmachines.com>
2021-08-27 16:56:10 +1200

    use ChromeHeadless instead of ChromeHeadlessCustom

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,14 +25,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessCustom'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true,
-    customLaunchers: {
-      ChromeHeadlessCustom: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu'],
-      },
-    },
   });
 };
