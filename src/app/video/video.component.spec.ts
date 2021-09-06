9f07b6cdeab706387f306adc50f10eb3cb6767e7
wadefleming-nz <81721082+wadefleming-nz@users.noreply.github.com>
2021-09-07 10:07:16 +1200

    test: Add tests for mic enabled & tokenserver inputs QUIC-779 (#47)

--- src/app/video/video.component.spec.ts
+++ src/app/video/video.component.spec.ts
@@ -1,19 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { VideoComponent } from './video.component';
 import { SMWebSDKService } from '../services/smwebsdk.service';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 
 describe('VideoComponent', () => {
-  let mockSMWebSdkService;
+  const mockSMWebSdkService = {
+    connected: false,
+    initialise: () => undefined,
+    connect: (_: string) => of(''),
+    registerEventCallbacks: () => undefined,
+    sendVideoBounds: () => undefined,
+    scene: {
+      startRecognize: () => Promise.resolve(),
+      stopRecognize: () => Promise.resolve(),
+    },
+  };
   let component: VideoComponent;
   let fixture: ComponentFixture<VideoComponent>;
 
   beforeEach(async () => {
-    mockSMWebSdkService = jasmine.createSpyObj<SMWebSDKService>('SMWebSDKService', {
-      initialise: undefined,
-      connect: of(''),
-    });
-
     await TestBed.configureTestingModule({
       declarations: [VideoComponent],
     }).compileComponents();
@@ -35,18 +40,100 @@ describe('VideoComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('inputs', () => {
-    describe('autoconnect', () => {
-      it('should call connect on the WebSDK when true and component is initialised', () => {
-        component.autoconnect = true;
-        fixture.detectChanges();
-        expect(mockSMWebSdkService.connect).toHaveBeenCalled();
+  describe('connect', () => {
+    let onConnectionSuccessSpy, onConnectionErrorSpy;
+
+    beforeEach(() => {
+      onConnectionSuccessSpy = spyOn<any>(component, 'onConnectionSuccess');
+      onConnectionErrorSpy = spyOn<any>(component, 'onConnectionError');
+      fixture.detectChanges();
+    });
+
+    describe('when connection is successful', () => {
+      beforeEach(() => {
+        component['connect']();
       });
 
-      it('should not call connect on the WebSDK when false and component is initialised', () => {
+      it('should call onConnectionSuccess', () => {
+        expect(onConnectionSuccessSpy).toHaveBeenCalled();
+      });
+
+      it('should not call onConnectionError ', () => {
+        expect(onConnectionErrorSpy).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('when connection is unsuccessful', () => {
+      beforeEach(() => {
+        spyOn(mockSMWebSdkService, 'connect').and.returnValue(throwError(null));
+        component['connect']();
+      });
+
+      it('should call onConnectionError', () => {
+        expect(onConnectionErrorSpy).toHaveBeenCalled();
+      });
+
+      it('should not call onConnectionSuccess', () => {
+        expect(onConnectionSuccessSpy).not.toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('inputs', () => {
+    describe('tokenserver', () => {
+      it('should be passed as a parameter to connect(..) in the WebSDK when a connection is started', () => {
+        const connectSpy = spyOn(mockSMWebSdkService, 'connect').and.callThrough();
+
+        component.tokenserver = 'test-token-server';
+        fixture.detectChanges();
+
+        component['connect']();
+        expect(connectSpy).toHaveBeenCalledOnceWith('test-token-server');
+      });
+    });
+
+    describe('autoconnect', () => {
+      it('should default to false', () => {
+        fixture.detectChanges();
+        expect(component.autoconnect).toBeFalse();
+      });
+
+      it('when true should call connect in the WebSDK when the component is initialised', () => {
+        const connectSpy = spyOn(mockSMWebSdkService, 'connect').and.callThrough();
+        component.autoconnect = true;
+        fixture.detectChanges();
+        expect(connectSpy).toHaveBeenCalled();
+      });
+
+      it('when false should not call connect in the WebSDK when the component is initialised', () => {
+        const connectSpy = spyOn(mockSMWebSdkService, 'connect').and.callThrough();
         component.autoconnect = false;
         fixture.detectChanges();
-        expect(mockSMWebSdkService.connect).not.toHaveBeenCalled();
+        expect(connectSpy).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('microphoneEnabled', () => {
+      it('when true should result in a call to startRecognize() in the WebSDK when a connection is successful', () => {
+        const startRecognizeSpy = spyOn(mockSMWebSdkService.scene, 'startRecognize');
+        mockSMWebSdkService.connected = true;
+
+        component.microphoneEnabled = 'true'; // TODO this takes a string and autoconnect takes a boolean
+        fixture.detectChanges();
+
+        component['onConnectionSuccess']();
+        expect(startRecognizeSpy).toHaveBeenCalled();
+      });
+
+      it('when false should result in a call to stopRecognize() in the WebSDK when a connection is successful', () => {
+        const stopRecognizeSpy = spyOn(mockSMWebSdkService.scene, 'stopRecognize');
+        mockSMWebSdkService.connected = true;
+
+        component.microphoneEnabled = 'false'; // TODO this takes a string and autoconnect takes a boolean
+        fixture.detectChanges();
+
+        component['onConnectionSuccess']();
+        expect(stopRecognizeSpy).toHaveBeenCalled();
       });
     });
   });
