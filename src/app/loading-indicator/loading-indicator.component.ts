704a676cb3d130331aeffd0b89a56aa6941bc7a5
wadefleming-nz <81721082+wadefleming-nz@users.noreply.github.com>
2021-09-14 15:43:47 +1200

    feat: embed SVG in component QUIC-910 (#58)

--- src/app/loading-indicator/loading-indicator.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-loading-indicator',
-  template: ` <img src="assets/spinner.svg" /> `,
-  styleUrls: ['./loading-indicator.component.scss'],
-})
-export class LoadingIndicatorComponent {}
