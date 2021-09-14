4600cd39d55a1e2cdd612782dcb1d55a8de43723
wadefleming-nz <81721082+wadefleming-nz@users.noreply.github.com>
2021-09-14 14:45:58 +1200

    feat: Add loading indicator component QUIC-897  (#55)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { createCustomElement } from '@angular/elements';
 import { VideoModule } from './video/video.module';
 import { VideoComponent } from './video/video.component';
+import { LoadingIndicatorModule } from './loading-indicator/loading-indicator.module';
+import { LoadingIndicatorComponent } from './loading-indicator/loading-indicator.component';
 
 @NgModule({
-  imports: [BrowserModule, HttpClientModule, VideoModule],
+  imports: [BrowserModule, HttpClientModule, VideoModule, LoadingIndicatorModule],
   declarations: [],
   bootstrap: [],
 })
 export class AppModule {
+  elements = [
+    { component: VideoComponent, name: 'sm-video' },
+    { component: LoadingIndicatorComponent, name: 'sm-loading-indicator' },
+  ];
+
   constructor(injector: Injector) {
-    const el = createCustomElement(VideoComponent, { injector: injector });
-    customElements.define('sm-video', el);
+    this.elements.map((element) => {
+      const el = createCustomElement(element.component, { injector: injector });
+      customElements.define(element.name, el);
+    });
   }
 
   public ngDoBootstrap() {}
---
+++ src/app/loading-indicator/loading-indicator.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-indicator',
+  template: ` <img src="assets/spinner.svg" /> `,
+  styleUrls: ['./loading-indicator.component.scss'],
+})
+export class LoadingIndicatorComponent {}
