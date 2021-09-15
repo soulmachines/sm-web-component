70d1048222dc2cb1e90cc346e0735448fce57ecc
Yvonne Yang <yvonne.yang@soulmachines.com>
2021-09-16 07:31:36 +1200

    use angular TestBed to setup service test

--- src/app/services/smwebsdk.service.spec.ts
+++ src/app/services/smwebsdk.service.spec.ts
@@ -1,15 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
 import { SMWebSDKService } from './smwebsdk.service';
 
 describe('SMWebSDKService', () => {
-  let service: SMWebSDKService;
-  let httpClientSpy: { get: jasmine.Spy };
+  let smwebsdkService: SMWebSDKService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
     httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
-    service = new SMWebSDKService(httpClientSpy as any);
+
+    TestBed.configureTestingModule({
+      providers: [SMWebSDKService, { provide: HttpClient, useValue: httpClientSpy }],
+    });
+
+    smwebsdkService = TestBed.inject(SMWebSDKService);
+    httpClientSpy = TestBed.inject(HttpClient) as jasmine.SpyObj<HttpClient>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(smwebsdkService).toBeTruthy();
   });
 });
