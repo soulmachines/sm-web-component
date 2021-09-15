d2572939e02fa783e85d096502cecd9d24818978
wadefleming-nz <81721082+wadefleming-nz@users.noreply.github.com>
2021-09-15 16:26:58 +1200

    test: automated tests for slot visibility QUIC-899 (#60)

--- src/app/video/video.component.spec.ts
+++ src/app/video/video.component.spec.ts
@@ -4,6 +4,7 @@ import { SMWebSDKService } from '../services/smwebsdk.service';
 import { of, throwError } from 'rxjs';
 import { Component, ViewChild } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { take, tap } from 'rxjs/operators';
 
 @Component({
   template: ` <app-video></app-video> `,
@@ -294,5 +295,70 @@ describe('VideoComponent', () => {
       component.child['onSpeechMarker'](null, data);
       expect(speechMarkerEmitSpy).toHaveBeenCalledWith(data);
     });
+  });
+
+  describe('connectingSubject', () => {
+    let connectingSubjectSpy;
+
+    beforeEach(() => {
+      createComponent();
+      fixture.detectChanges();
+    });
+
+    it('should default to false', (done) => {
+      component.child.connectingSubject
+        .pipe(
+          take(1),
+          tap((value) => {
+            expect(value).toBeFalse();
+            done();
+          }),
+        )
+        .subscribe();
+    });
+
+    describe('emitter', () => {
+      beforeEach(() => {
+        connectingSubjectSpy = spyOn(component.child.connectingSubject, 'next');
+      });
+
+      it('should emit true when the connection is initialised', () => {
+        component.child['connect']();
+        expect(connectingSubjectSpy).toHaveBeenCalledWith(true);
+      });
+
+      it('should emit false when the connection is successful', () => {
+        component.child['onConnectionSuccess']();
+        expect(connectingSubjectSpy).toHaveBeenCalledWith(false);
+      });
+
+      it('should emit false when the connection errors', () => {
+        component.child['onConnectionError']('error');
+        expect(connectingSubjectSpy).toHaveBeenCalledWith(false);
+      });
+    });
+  });
+
+  describe('connecting indicator slot', () => {
+    beforeEach(() => {
+      createComponent();
+      fixture.detectChanges();
+    });
+
+    [
+      { subjectValue: true, slotVisibility: true, slotVisibilityText: 'shown' },
+      { subjectValue: false, slotVisibility: false, slotVisibilityText: 'hidden' },
+    ].forEach(({ subjectValue, slotVisibility, slotVisibilityText }) =>
+      it(`should be ${slotVisibilityText} when connectingSubject is ${subjectValue}`, () => {
+        component.child.connectingSubject.next(subjectValue);
+        fixture.detectChanges();
+
+        const videoElement = fixture.debugElement.query(By.css('app-video'));
+        const shadowRoot: DocumentFragment = videoElement.nativeElement.shadowRoot;
+        const slotElement = shadowRoot.querySelector('slot');
+
+        expect(Boolean(slotElement)).toBe(slotVisibility);
+      }),
+    );
   });
 });
