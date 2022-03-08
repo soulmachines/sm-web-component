f7486e5277887b3a8f76ff4496a877f0ffa6c74e
Tanya Gray <153077+tanyagray@users.noreply.github.com>
2022-03-09 11:19:27 +1300

    remove script from embed code demo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  embedCodeUrl: 'https://static.soulmachines.com/widget-1.0.0.min.js',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: false,
+  embedCodeUrl: 'http://localhost:4200/widget.js',
 };
