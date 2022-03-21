e57f8b0926623e1bc47c73f683b85ee91aa9173b
Tanya Gray <153077+tanyagray@users.noreply.github.com>
2022-03-21 17:18:05 +1300

    refactor: Serve examples folder using ParcelJS QUIC-1573

---
+++ src/soulmachines.js
@@ -0,0 +1,8 @@
+/**
+ * The npm command `start:app` or `build:app` must have
+ * been run for these files to exist.
+ */
+import '../dist/app/main-es2015';
+import '../dist/app/polyfills-es2015';
+import '../dist/app/runtime-es2015';
+import '../dist/app/vendor-es2015';
