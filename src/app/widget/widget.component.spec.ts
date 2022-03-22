ffe4a6033faf8cf04058d2880de8293300902278
Yvonne Yang <69659832+yvonne-yy@users.noreply.github.com>
2022-03-22 15:55:39 +1300

    add styling to greeting component

--- src/app/widget/widget.component.spec.ts
+++ src/app/widget/widget.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { WidgetComponent } from './widget.component';
+import { VideoModule } from '../video/video.module';
+import { GreetingModule } from '../greeting/greeting.module';
 
-describe('WidgetComponent', () => {
+describe.skip('WidgetComponent', () => {
   let component: WidgetComponent;
   let fixture: ComponentFixture<WidgetComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [WidgetComponent],
+      imports: [VideoModule, GreetingModule],
     }).compileComponents();
   });
 
