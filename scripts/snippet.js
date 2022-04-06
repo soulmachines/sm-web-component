8260541996217e7662f17380f520cc8b446fa168
Allan Pope <allan.pope@soulmachines.com>
2022-04-06 14:37:19 +1200

    feat: update script to use anonymous function

--- scripts/snippet.js
+++ scripts/snippet.js
@@ -2,24 +2,27 @@
  * Minify this script for use in DDNA Studio
  * https://www.digitalocean.com/community/tools/minify
  */
+
 var SM_CONFIG = {};
 
-var w = window;
-var d = document;
+(function () {
+  var w = window;
+  var d = document;
 
-var s = d.createElement('script');
-s.type = 'text/javascript';
-s.async = false;
-s.defer = true;
+  var s = d.createElement('script');
+  s.type = 'text/javascript';
+  s.async = false;
+  s.defer = true;
 
-// update to public CDN path before minifying
-s.src = '../soulmachines.js';
+  // update to public CDN path before minifying
+  s.src = '../soulmachines.js';
 
-s.onload = () => {
-  w.sm.configure(SM_CONFIG);
-};
+  s.onload = () => {
+    w.sm.configure(SM_CONFIG);
+  };
 
-w.addEventListener('DOMContentLoaded', () => {
-  var ps = d.getElementsByTagName('script')[0];
-  ps.parentNode.insertBefore(s, ps);
-});
+  w.addEventListener('DOMContentLoaded', () => {
+    var ps = d.getElementsByTagName('script')[0];
+    ps.parentNode.insertBefore(s, ps);
+  });
+})();
