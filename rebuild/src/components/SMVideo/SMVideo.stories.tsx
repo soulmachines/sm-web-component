f660f24210f3a7df67ef9683b932977ca008e716
Allan Pope <allan.pope@soulmachines.com>
2022-04-07 15:31:41 +1200

    feat: add seperate story for token server

--- rebuild/src/components/SMVideo/SMVideo.stories.tsx
+++ rebuild/src/components/SMVideo/SMVideo.stories.tsx
@@ -5,7 +5,12 @@ export default {
   component: SMVideo,
 };
 
-export const Basic = ({ apiKey }: SMVideoProps) => <SMVideo apiKey={apiKey} />;
-Basic.args = {
+export const ApiKey = ({ apiKey }: SMVideoProps) => <SMVideo apiKey={apiKey} />;
+ApiKey.args = {
   apiKey: import.meta.env.VITE__PROJECT_API_KEY,
+};
+
+export const TokenServer = ({ tokenServer }: SMVideoProps) => <SMVideo tokenServer={tokenServer} />;
+TokenServer.args = {
+  tokenServer: import.meta.env.VITE__TOKEN_SERVER,
 };
