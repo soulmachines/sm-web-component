39d52b35fca77a23b00d95ad9b167a1c2c63ec15
Allan Pope <allan.pope@soulmachines.com>
2022-04-11 13:49:09 +1200

    fix: mock smwebsdk so it doesnt try to connect

--- rebuild/src/components/SMVideo/SMVideo.test.tsx
+++ rebuild/src/components/SMVideo/SMVideo.test.tsx
@@ -1,6 +1,8 @@
 import { render } from '@testing-library/preact';
 import { SMVideo } from '.';
 
+jest.mock('@soulmachines/smwebsdk');
+
 describe('<SMVideo />', () => {
   it('renders a loading indicator', () => {
     const { getByText } = render(<SMVideo apiKey="123" connecting-indicator={<p>Loading...</p>} />);
