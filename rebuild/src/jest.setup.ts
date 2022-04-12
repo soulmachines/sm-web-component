b4b683dc87b5cf7a1c1bfe6e456dc188e9007dd3
Allan Pope <allan.pope@soulmachines.com>
2022-04-13 09:18:55 +1200

    feat: update video bounds on resize QUIC-1655

--- rebuild/src/jest.setup.ts
+++ rebuild/src/jest.setup.ts
@@ -5,3 +5,12 @@ import '@testing-library/jest-dom/extend-expect';
 // WebSDK requires crypto. Need to add it manually
 import { Crypto } from '@peculiar/webcrypto';
 window.crypto = new Crypto();
+
+Object.defineProperty(window, 'ResizeObserver', {
+  writable: true,
+  value: jest.fn().mockImplementation(() => ({
+    disconnect: jest.fn(),
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+  })),
+});
