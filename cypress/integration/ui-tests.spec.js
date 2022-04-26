2b12e13147fc42bf3b1eef5609a3ce1951de8292
Thabo <thabo.dube@soulmachines.com>
2022-04-26 14:06:20 +1200

    fix: Prettier formatting

--- cypress/integration/ui-tests.spec.js
+++ cypress/integration/ui-tests.spec.js
@@ -1,16 +1,14 @@
-
-
 describe('Verify UI elements within the widget', () => {
   beforeEach(() => {
-    cy.visit('/')
-    cy.wait(200)
-  })
+    cy.visit('/');
+    cy.wait(200);
+  });
 
   it('launches a scene and disconnects validating UI elements', () => {
-    cy.close_greeting()
-    cy.launch_scene()
-    cy.mute_scene()
-    cy.unmute_scene()
-    cy.disconnect_scene()
-  })
-})
+    cy.close_greeting();
+    cy.launch_scene();
+    cy.mute_scene();
+    cy.unmute_scene();
+    cy.disconnect_scene();
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,44 +1,29 @@
-//This command launches a scene 
+//This command launches a scene
 Cypress.Commands.add('launch_scene', () => {
-    cy.get('app-profile-picture')
-    .should('be.visible')
-    .click()
-    cy.wait(15000)
+  cy.get('app-profile-picture').should('be.visible').click();
+  cy.wait(15000);
 });
 
 //This command Disconnects from a scene
 Cypress.Commands.add('disconnect_scene', () => {
-    cy.get('app-icon[name=close]')
-    .should('be.visible')
-    .click()
-    cy.wait(500)
-    cy.get('app-profile-picture')
-    .should('be.visible')
-})
+  cy.get('app-icon[name=close]').should('be.visible').click();
+  cy.wait(500);
+  cy.get('app-profile-picture').should('be.visible');
+});
 
 //This command mutes the scene by click on the mute button
-Cypress.Commands.add('mute_scene', () =>{
-    cy.get('button[class=mic-button]')
-    .should('be.visible')
-    .click()
-})
+Cypress.Commands.add('mute_scene', () => {
+  cy.get('button[class=mic-button]').should('be.visible').click();
+});
 
 //This command unmutes the scene by clicking the mute button
-Cypress.Commands.add('unmute_scene', () =>{
-    cy.get('sm-widget > div > div > button.mic-button.off')
-    .should('be.visible')
-    .click()
-})
+Cypress.Commands.add('unmute_scene', () => {
+  cy.get('sm-widget > div > div > button.mic-button.off').should('be.visible').click();
+});
 
 //This command closes the greeting message container
-Cypress.Commands.add('close_greeting', () =>{
-    cy.get('div[class=greeting-container]')
-    .should('be.visible')
-    cy.get('app-greeting > div > button')
-    .should('be.visible')
-    .click()
-    cy.get('div[class=greeting-container]')
-    .should('not.exist')
-})
-
-
+Cypress.Commands.add('close_greeting', () => {
+  cy.get('div[class=greeting-container]').should('be.visible');
+  cy.get('app-greeting > div > button').should('be.visible').click();
+  cy.get('div[class=greeting-container]').should('not.exist');
+});
