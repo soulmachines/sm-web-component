faf30b29b37b21e916808e38e80050bb2c39d744
Thabo <thabo.dube@soulmachines.com>
2022-05-02 14:50:24 +1200

    fix: code formatting fix

--- cypress/integration/ui-tests.spec.js
+++ cypress/integration/ui-tests.spec.js
@@ -9,15 +9,15 @@ describe('Verify UI elements within the widget', () => {
     cy.get('app-profile-picture').should('be.visible');
   });
 
-  it('validates elements when scene is connected', () =>{
+  it('validates elements when scene is connected', () => {
     cy.launchScene();
     cy.muteScene();
     //cy.unmuteScene();
-  })
+  });
 
-  it('validates elements when scene is disconnected', () =>{
+  it('validates elements when scene is disconnected', () => {
     cy.launchScene();
     cy.disconnectScene();
     cy.get('app-profile-picture').should('be.visible');
-  })
+  });
 });
