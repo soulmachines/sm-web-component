93df57fca3c07fdbb7e1912241958d379fe5d019
Thabo <thabo.dube@soulmachines.com>
2022-05-02 14:44:42 +1200

    fix: configured cypress to new app url

--- cypress/integration/ui-tests.spec.js
+++ cypress/integration/ui-tests.spec.js
@@ -4,11 +4,20 @@ describe('Verify UI elements within the widget', () => {
     cy.wait(200);
   });
 
-  it('launches a scene and disconnects validating UI elements', () => {
+  it('validates elements when scene is not running', () => {
     cy.closeGreeting();
+    cy.get('app-profile-picture').should('be.visible');
+  });
+
+  it('validates elements when scene is connected', () =>{
     cy.launchScene();
     cy.muteScene();
     //cy.unmuteScene();
+  })
+
+  it('validates elements when scene is disconnected', () =>{
+    cy.launchScene();
     cy.disconnectScene();
-  });
+    cy.get('app-profile-picture').should('be.visible');
+  })
 });
