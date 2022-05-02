1768a9d8567c508c61a14c8ed62142cf9fc0ee6d
Allan Pope <allan.pope@soulmachines.com>
2022-05-03 08:51:23 +1200

    fix: bump preactement and remove workaround

--- rebuild/src/components/SMVideo/SMVideo.tsx
+++ rebuild/src/components/SMVideo/SMVideo.tsx
@@ -6,18 +6,16 @@ export type SMVideoProps = {
   apiKey?: string;
   tokenServer?: string;
   autoConnect: string;
-  // Slot attributes come through as the case entered
-  // Have asked about auto formatting them to camelcase like the attributes https://github.com/jahilldev/component-elements/issues/16
-  'connecting-indicator'?: JSX.Element;
+  connectingIndicator?: JSX.Element;
 };
 
-export function SMVideo(props: SMVideoProps) {
+export function SMVideo({ autoConnect, tokenServer, apiKey, connectingIndicator }: SMVideoProps) {
   // Cast from string into a boolean
-  const autoConnect = props.autoConnect === 'true';
+  const isAutoConnect = autoConnect === 'true';
 
   return (
-    <SoulMachinesProvider apiKey={props.apiKey} tokenServer={props.tokenServer}>
-      <Video autoConnect={autoConnect} loadingIndicator={props['connecting-indicator']} />
+    <SoulMachinesProvider apiKey={apiKey} tokenServer={tokenServer}>
+      <Video autoConnect={isAutoConnect} loadingIndicator={connectingIndicator} />
     </SoulMachinesProvider>
   );
 }
