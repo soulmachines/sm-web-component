fb4a734cb68f3828c42f321473fb9083c63a7afc
Thabo <thabo.dube@soulmachines.com>
2022-05-05 09:57:07 +1200

    test; made ui tests more structured

--- cypress/integration/ui-tests.spec.js
+++ cypress/integration/ui-tests.spec.js
@@ -1,25 +1,34 @@
 describe('Verify UI elements within the widget', () => {
+  before(() => {
+    cy.visit('/');
+    cy.wait(200);
+    cy.launchScene();
+  });
+
+  it('checks that the mute button is rendered', () => {
+    cy.unmuteScene();
+    cy.muteScene();
+  });
+
+  it('checks that the camera button is rendered', () => {
+    cy.cameraOn();
+    cy.cameraOff();
+  });
+
+  it('checks that the scene cane be disconnected', () => {
+    cy.disconnectScene();
+    cy.get('app-profile-picture').should('be.visible');
+  });
+});
+
+describe('When scene is not connected', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.wait(200);
   });
 
-  it('validates elements when scene is not running', () => {
+  it('checks that greeting and profile picture are rendered', () => {
     cy.closeGreeting();
-    cy.get('app-profile-picture').should('be.visible');
-  });
-
-  it('validates elements when scene is connected', () => {
-    cy.launchScene();
-    cy.unmuteScene();
-    cy.muteScene();
-    cy.cameraOn();
-    cy.cameraOff();
-  });
-
-  it('validates elements when scene is disconnected', () => {
-    cy.launchScene();
-    cy.disconnectScene();
     cy.get('app-profile-picture').should('be.visible');
   });
 });
