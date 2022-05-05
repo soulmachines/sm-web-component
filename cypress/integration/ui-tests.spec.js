2961cf60660fb52f6a108d44b8073b5525bc2eff
Allan Pope <allan.pope@soulmachines.com>
2022-05-06 10:19:22 +1200

    feat: responsive design widget quic-1687 (#172)
    * feat: render sm-widget with no styles
    
    * feat: fade out greeting on close
    
    * feat: using this to test out different cases
    
    * refactor: css
    
    * feat: responsive styles
    
    * fix: video styling
    
    * feat: add examples
    
    * fix: rendering issue in safari
    
    * fix: safari video bug
    
    * fix: inform browser of transition
    
    * feat: add cards
    
    * feat: add hover to connect button
    
    * feat: tweak loader
    
    * fix: spelling
    
    * chore: add auto generated file
    
    * refactor: use same setup for both tests
    
    * fix: update selector as it is now hidden
    
    * fix: remove wait

--- cypress/integration/ui-tests.spec.js
+++ cypress/integration/ui-tests.spec.js
@@ -1,50 +1,49 @@
-describe('When a scene is connected', () => {
+describe('scene', () => {
   before(() => {
     cy.visit('/');
-    cy.wait(200);
-    cy.launchScene();
   });
 
-  it('checks that the profile picture is not rendered', () => {
-    cy.get('app-profile-picture').should('not.be.visible');
+  describe('when it is connected', () => {
+    before(() => {
+      cy.launchScene();
+    });
+
+    it('checks that the profile picture is not rendered', () => {
+      cy.get('app-profile-picture').should('not.be.visible');
+    });
+
+    it('checks that the mute button is rendered', () => {
+      cy.unmuteScene();
+      cy.muteScene();
+    });
+
+    it('checks that the camera button is rendered', () => {
+      cy.cameraOn();
+      cy.cameraOff();
+    });
+
+    it('checks that the scene can be disconnected', () => {
+      cy.disconnectScene();
+      cy.get('app-profile-picture').should('be.visible');
+    });
   });
 
-  it('checks that the mute button is rendered', () => {
-    cy.unmuteScene();
-    cy.muteScene();
-  });
+  describe('when it is not connected', () => {
+    it('checks if greeting is rendered and closes it', () => {
+      cy.get('.greeting-container').should('be.visible');
+      cy.closeGreeting();
+    });
 
-  it('checks that the camera button is rendered', () => {
-    cy.cameraOn();
-    cy.cameraOff();
-  });
+    it('does not render a camera button', () => {
+      cy.get('sm-widget .camera-button.inactive').should('not.be.visible');
+    });
 
-  it('checks that the scene cane be disconnected', () => {
-    cy.disconnectScene();
-    cy.get('app-profile-picture').should('be.visible');
-  });
-});
+    it('does not render a mute button', () => {
+      cy.get('sm-widget .mic-button.inactive').should('not.be.visible');
+    });
 
-describe('When scene is not connected', () => {
-  beforeEach(() => {
-    cy.visit('/');
-    cy.wait(200);
-  });
-
-  it('checks if greeting is rendered and closes it', () => {
-    cy.get('.greeting-container').should('be.visible');
-    cy.closeGreeting();
-  });
-
-  it('does not render a camera button', () => {
-    cy.get('sm-widget .camera-button.inactive').should('not.be.visible');
-  });
-
-  it('does not render a mute button', () => {
-    cy.get('sm-widget .mic-button.inactive').should('not.be.visible');
-  });
-
-  it('checks that the profile picture is rendered', () => {
-    cy.get('app-profile-picture').should('be.visible');
+    it('checks that the profile picture is rendered', () => {
+      cy.get('app-profile-picture').should('be.visible');
+    });
   });
 });
