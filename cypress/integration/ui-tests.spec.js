252d9f9c7ed2d122bebf2b12cb0d345db1cc0627
Thabo Dube <52473519+thaboBD@users.noreply.github.com>
2022-05-05 15:23:42 +1200

    test: adds test for camera button quic-1707 (#171)
    * test: adds test for camera button
    
    * fix: run prettier formmating
    
    * test; made ui tests more structured
    
    * fix: added title tag for accessibility
    
    * test: expanded test coverage
    
    * fix: increased timeout by 10secs
    
    * fix: update test decription and add a new assertion
    
    * test: add profile pictures tests
    
    * fix: code formatting
    
    * Update cypress/integration/ui-tests.spec.js
    
    Co-authored-by: Allan Pope <allan.pope@soulmachines.com>

--- cypress/integration/ui-tests.spec.js
+++ cypress/integration/ui-tests.spec.js
@@ -1,23 +1,50 @@
-describe('Verify UI elements within the widget', () => {
+describe('When a scene is connected', () => {
+  before(() => {
+    cy.visit('/');
+    cy.wait(200);
+    cy.launchScene();
+  });
+
+  it('checks that the profile picture is not rendered', () => {
+    cy.get('app-profile-picture').should('not.be.visible');
+  });
+
+  it('checks that the mute button is rendered', () => {
+    cy.unmuteScene();
+    cy.muteScene();
+  });
+
+  it('checks that the camera button is rendered', () => {
+    cy.cameraOn();
+    cy.cameraOff();
+  });
+
+  it('checks that the scene cane be disconnected', () => {
+    cy.disconnectScene();
+    cy.get('app-profile-picture').should('be.visible');
+  });
+});
+
+describe('When scene is not connected', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.wait(200);
   });
 
-  it('validates elements when scene is not running', () => {
+  it('checks if greeting is rendered and closes it', () => {
+    cy.get('.greeting-container').should('be.visible');
     cy.closeGreeting();
-    cy.get('app-profile-picture').should('be.visible');
   });
 
-  it('validates elements when scene is connected', () => {
-    cy.launchScene();
-    cy.muteScene();
-    //cy.unmuteScene();  Commented because the framework was build before the camera icon was added. after the fist merge a locator for the cam icon needs to be added and this one updated
+  it('does not render a camera button', () => {
+    cy.get('sm-widget .camera-button.inactive').should('not.be.visible');
   });
 
-  it('validates elements when scene is disconnected', () => {
-    cy.launchScene();
-    cy.disconnectScene();
+  it('does not render a mute button', () => {
+    cy.get('sm-widget .mic-button.inactive').should('not.be.visible');
+  });
+
+  it('checks that the profile picture is rendered', () => {
     cy.get('app-profile-picture').should('be.visible');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,5 @@
 //This command launches a scene
-Cypress.Commands.add('launchScene', (timeout = 25000) => {
+Cypress.Commands.add('launchScene', (timeout = 35000) => {
   cy.get('app-profile-picture').should('be.visible').click();
   cy.get('.close-button', { timeout }).should('be.visible');
 });
@@ -13,17 +13,33 @@ Cypress.Commands.add('disconnectScene', () => {
 
 //This command mutes the scene by click on the mute button
 Cypress.Commands.add('muteScene', () => {
-  cy.get('.mic-button').should('be.visible').click();
+  cy.get('sm-widget .mic-button').should('be.visible').click();
+  cy.contains('micOn').should('not.exist');
+  cy.contains('mic').should('exist');
 });
 
 //This command unmutes the scene by clicking the mute button
 Cypress.Commands.add('unmuteScene', () => {
-  cy.get('sm-widget .mic-button.off').should('be.visible').click();
+  cy.get('sm-widget .mic-button.inactive').should('be.visible').click();
+  cy.contains('micOff').should('not.exist');
+  cy.contains('mic').should('exist');
+});
+
+//This command turns on the camera
+Cypress.Commands.add('cameraOn', () => {
+  cy.get('.camera-button').should('be.visible').click();
+  cy.contains('cameraOff').should('not.exist');
+  cy.contains('camera').should('exist');
+});
+
+Cypress.Commands.add('cameraOff', () => {
+  cy.get('sm-widget .camera-button').should('be.visible').click();
+  cy.contains('cameraOn').should('not.exist');
+  cy.contains('camera').should('exist');
 });
 
 //This command closes the greeting message container
 Cypress.Commands.add('closeGreeting', () => {
-  cy.get('div[class=greeting-container]', { timeout: 10000 }).should('be.visible');
   cy.get('app-greeting button').should('be.visible').click();
   cy.get('.greeting-container').should('not.exist');
 });
