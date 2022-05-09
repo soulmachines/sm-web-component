73e6762ff3f76d11281c7c2ad2ca492d55ea7a1f
Allan Pope <allan.pope@soulmachines.com>
2022-05-10 10:55:46 +1200

    fix: use preact in storybook quic-1718 (#177)

--- rebuild/.storybook/main.js
+++ rebuild/.storybook/main.js
@@ -1,11 +1,20 @@
+const { mergeConfig } = require('vite');
+
 module.exports = {
   // Function that allows vite config overrides
   async viteFinal(config) {
-    config.esbuild = {
-      jsxInject: `import { h } from 'preact'`,
-    };
-
-    return config;
+    return mergeConfig(config, {
+      // customize the Vite config here
+      esbuild: {
+        jsxInject: `import { h } from 'preact'`,
+      },
+      resolve: {
+        alias: {
+          react: 'preact/compat',
+          'react-dom': 'preact/compat',
+        },
+      },
+    });
   },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-essentials'],
