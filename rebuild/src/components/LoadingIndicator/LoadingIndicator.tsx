70ebda00913c1e0f2211bcffbe6db10ac8d6e083
Allan Pope <allan.pope@soulmachines.com>
2022-05-10 14:19:08 +1200

    fix: loading size

--- rebuild/src/components/LoadingIndicator/LoadingIndicator.tsx
+++ rebuild/src/components/LoadingIndicator/LoadingIndicator.tsx
@@ -1,10 +1,14 @@
-export function LoadingIndicator() {
+export type LoadingIndicatorProps = {
+  size?: number | string;
+};
+
+export function LoadingIndicator({ size }: LoadingIndicatorProps) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
-      width="100%"
-      height="100%"
+      width={size || '100%'}
+      height={size || '100%'}
       viewBox="0 0 100 100"
       preserveAspectRatio="xMidYMid"
       fill="currentcolor"
