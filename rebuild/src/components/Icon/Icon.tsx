61837e547ea939685262752903c29e5eda60cbb0
Allan Pope <allan.pope@soulmachines.com>
2022-05-10 13:42:43 +1200

    feat: allow a custom title to be passed through

--- rebuild/src/components/Icon/Icon.tsx
+++ rebuild/src/components/Icon/Icon.tsx
@@ -2,10 +2,11 @@ import paths from './paths';
 
 export type IconProps = {
   name: keyof typeof paths;
+  title?: string;
   size?: number;
 };
 
-export function Icon({ size, name }: IconProps) {
+export function Icon({ size, name, title }: IconProps) {
   return (
     <svg
       width={size}
@@ -15,7 +16,7 @@ export function Icon({ size, name }: IconProps) {
       xmlns="http://www.w3.org/2000/svg"
       fill-rule="evenodd"
     >
-      <title>{name}</title>
+      <title>{title || name}</title>
       {paths[name].map((path) => (
         <path d={path} />
       ))}
