e37447032e7d331ee9fad93fee46737dce867d19
Allan Pope <allan.pope@soulmachines.com>
2022-05-18 14:33:39 +1200

    feat: allow mic to be toggled on/off

--- rebuild/src/components/VideoControls/VideoControls.test.tsx
+++ rebuild/src/components/VideoControls/VideoControls.test.tsx
@@ -2,10 +2,19 @@ import { fireEvent, render } from '@testing-library/preact';
 import { VideoControls } from '.';
 
 const mockDisconnect = jest.fn();
+const mockToggleMicrophone = jest.fn();
+let mockIsMicrophoneEnabled: boolean;
 
 jest.mock('../../contexts/SoulMachinesContext', () => ({
   useSoulMachines: () => ({
     disconnect: mockDisconnect,
+  }),
+}));
+
+jest.mock('../../contexts/SoulMachinesMedia', () => ({
+  useSMMedia: () => ({
+    isMicrophoneEnabled: mockIsMicrophoneEnabled,
+    toggleMicrophone: mockToggleMicrophone,
   }),
 }));
 
@@ -16,6 +25,12 @@ describe('<VideoControls />', () => {
     expect(mockDisconnect).toBeCalledTimes(0);
   });
 
+  it('does not call toggleMicrophone on the initial render', () => {
+    render(<VideoControls />);
+
+    expect(mockToggleMicrophone).toBeCalledTimes(0);
+  });
+
   it('calls disconnect when the disconnect button is clicked', async () => {
     const { getByTitle } = render(<VideoControls />);
     const button = getByTitle('Close video');
@@ -23,5 +38,33 @@ describe('<VideoControls />', () => {
     await fireEvent.click(button);
 
     expect(mockDisconnect).toBeCalledTimes(1);
+  });
+
+  describe('when microphone is disabled', () => {
+    beforeEach(() => {
+      mockIsMicrophoneEnabled = false;
+    });
+
+    it('calls toggleMicrophone when the enable button is clicked', async () => {
+      const { getByTitle } = render(<VideoControls />);
+      const enableButton = getByTitle('Enable microphone');
+      await fireEvent.click(enableButton);
+
+      expect(mockToggleMicrophone).toBeCalledTimes(1);
+    });
+  });
+
+  describe('when microphone is enabled', () => {
+    beforeEach(() => {
+      mockIsMicrophoneEnabled = true;
+    });
+
+    it('calls toggleMicrophone when the disable button is clicked', async () => {
+      const { getByTitle } = render(<VideoControls />);
+      const disableButton = getByTitle('Disable microphone');
+      await fireEvent.click(disableButton);
+
+      expect(mockToggleMicrophone).toBeCalledTimes(1);
+    });
   });
 });
--- rebuild/src/components/VideoControls/VideoControls.tsx
+++ rebuild/src/components/VideoControls/VideoControls.tsx
@@ -1,12 +1,23 @@
 import { useSoulMachines } from '../../contexts/SoulMachinesContext';
+import { useSMMedia } from '../../contexts/SoulMachinesMedia';
 import { Icon } from '../Icon';
 
 export function VideoControls() {
   const { disconnect } = useSoulMachines();
+  const { toggleMicrophone, isMicrophoneEnabled } = useSMMedia();
+
+  const microphoneIcon = isMicrophoneEnabled ? 'mic' : 'micOff';
+  const microphoneText = isMicrophoneEnabled ? 'Disable microphone' : 'Enable microphone';
 
   return (
-    <button onClick={disconnect}>
-      <Icon name="close" title="Close video" />
-    </button>
+    <div>
+      <button onClick={disconnect}>
+        <Icon name="close" title="Close video" />
+      </button>
+
+      <button onClick={toggleMicrophone}>
+        <Icon name={microphoneIcon} title={microphoneText} />
+      </button>
+    </div>
   );
 }
