c3f31c056944f70f339ffcf15a4af4da9796a655
Allan Pope <allan.pope@soulmachines.com>
2022-05-20 12:29:15 +1200

    feat: position close button

--- rebuild/src/components/Card/Card.tsx
+++ rebuild/src/components/Card/Card.tsx
@@ -17,7 +17,10 @@ export function Card({ children }: CardProps) {
     <div className="sm-bg-white sm-rounded-xl sm-relative sm-max-w-xs sm-w-full sm-px-6 sm-py-4 sm-shadow-lg">
       {children}
 
-      <button onClick={() => setIsHidden(true)}>
+      <button
+        onClick={() => setIsHidden(true)}
+        className="sm-absolute sm-top-0 sm-right-0 sm-translate-x-1/2 -sm-translate-y-2/4"
+      >
         <Icon name="close" title="Hide card" />
       </button>
     </div>
