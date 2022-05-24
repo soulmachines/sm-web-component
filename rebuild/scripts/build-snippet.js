635d18c4dbe28f0846f640e93a20311d5cd34141
Allan Pope <allan.pope@soulmachines.com>
2022-05-25 11:11:08 +1200

    fix: set node env

--- rebuild/scripts/build-snippet.js
+++ rebuild/scripts/build-snippet.js
@@ -1,11 +1,20 @@
 try {
+  require('dotenv').config();
   const shell = require('shelljs');
   const manifest = require('../dist/manifest.json');
-
+  const isProduction = process.env.NODE_ENV === 'production';
+  const CDN_URL = 'https://static.soulmachines.com/';
   const { file, css } = manifest['src/web-components/sm-widget/index.tsx'];
+  let jsFileName = file;
+  let cssFileNames = css;
+
+  if (isProduction) {
+    jsFileName = CDN_URL + file;
+    cssFileNames = css.map((name) => CDN_URL + name).join(',');
+  }
 
   shell.exec(
-    `npx plop widget-snippet --plopfile generators/index.js -- --javascriptFileName ${file} --cssFileNames ${css}`,
+    `npx plop widget-snippet --plopfile generators/index.js -- --javascriptFileName ${jsFileName} --cssFileNames ${cssFileNames}`,
   );
 } catch (error) {
   console.error(`Something went wrong: ${error}`);
