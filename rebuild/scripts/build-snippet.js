5acca67f5cb5b14a37095d0a1b51f6e48beff921
Allan Pope <allan.pope@soulmachines.com>
2022-05-25 15:18:51 +1200

    feat: build and push to a dev folder

--- rebuild/scripts/build-snippet.js
+++ rebuild/scripts/build-snippet.js
@@ -2,16 +2,10 @@ try {
   require('dotenv').config();
   const shell = require('shelljs');
   const manifest = require('../dist/manifest.json');
-  const isProduction = process.env.NODE_ENV === 'production';
-  const CDN_URL = 'https://static.soulmachines.com/';
+  const CDN_URL = process.env.CDN_ENDPOINT;
   const { file, css } = manifest['src/web-components/index.ts'];
-  let jsFileName = file;
-  let cssFileNames = css;
-
-  if (isProduction) {
-    jsFileName = CDN_URL + file;
-    cssFileNames = css.map((name) => CDN_URL + name).join(',');
-  }
+  let jsFileName = CDN_URL + file;
+  let cssFileNames = css.map((name) => CDN_URL + name).join(',');
 
   shell.exec(
     `npx plop widget-snippet --plopfile generators/index.js -- --javascriptFileName ${jsFileName} --cssFileNames ${cssFileNames}`,
