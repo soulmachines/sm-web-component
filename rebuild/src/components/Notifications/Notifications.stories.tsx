607f4b099dd4d8fa9503dad57baeedb126c55369
Allan Pope <allan.pope@soulmachines.com>
2022-06-08 13:12:13 +1200

    fix: update broken stories

--- rebuild/src/components/Notifications/Notifications.stories.tsx
+++ rebuild/src/components/Notifications/Notifications.stories.tsx
@@ -1,8 +1,10 @@
 import { Notifications } from '.';
+import { SMProviderDecorator } from '../../../.storybook/decorators/SMProviderDecorator';
 
 export default {
   title: `Components / Notifications`,
   component: Notifications,
+  decorators: [SMProviderDecorator],
 };
 
 export const Basic = () => <Notifications />;
