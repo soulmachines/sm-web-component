871c5c84979d4d038fa76ede04a527909ae25b6e
Allan Pope <allan.pope@soulmachines.com>
2022-06-08 14:08:32 +1200

    fix: warning lints

--- rebuild/src/components/BindPublicSmEvents/BindPublicSmEvents.tsx
+++ rebuild/src/components/BindPublicSmEvents/BindPublicSmEvents.tsx
@@ -3,38 +3,42 @@ import { useSoulMachines } from '../../contexts/SoulMachinesContext';
 import { ConnectionStatus } from '../../enums';
 
 export interface WebComponentElement extends HTMLElement {
-  sendTextMessage?: Function;
+  sendTextMessage?: (message: string) => void;
 }
 
 export type BindPublicSmEventsProps = {
   element: WebComponentElement;
 };
 
+type GenericFunction = (args: unknown) => void;
+
 export function BindPublicSmEvents({ element }: BindPublicSmEventsProps) {
-  const htmlElement = element as unknown as Record<string, Function | undefined>;
+  const htmlElement = element as unknown as Record<string, GenericFunction | undefined>;
   const { persona, connectionStatus } = useSoulMachines();
-  const sendTextMessage = (text: string) => {
-    persona.conversationSend(text, {}, {});
-  };
-  const publicMethods: [string, Function][] = [['sendTextMessage', sendTextMessage]];
-  const addPublicMethods = () => {
-    publicMethods.map(([name, implementation]) => {
-      htmlElement[name] = implementation;
-    });
-  };
-  const removePublicMethods = () => {
-    publicMethods.map(([name]) => {
-      htmlElement[name] = undefined;
-    });
-  };
 
   useEffect(() => {
+    const sendTextMessage = (text: string) => {
+      persona.conversationSend(text, {}, {});
+    };
+    const publicMethods: [string, GenericFunction][] = [['sendTextMessage', sendTextMessage]];
+
+    const addPublicMethods = () => {
+      publicMethods.map(([name, implementation]) => {
+        htmlElement[name] = implementation;
+      });
+    };
+    const removePublicMethods = () => {
+      publicMethods.map(([name]) => {
+        htmlElement[name] = undefined;
+      });
+    };
+
     if (connectionStatus === ConnectionStatus.CONNECTED) {
       addPublicMethods();
     } else {
       removePublicMethods();
     }
-  }, [connectionStatus]);
+  }, [connectionStatus, htmlElement, persona]);
 
   return null;
 }
