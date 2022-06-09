91222081d1e1fc04f4119a5c0907305177c6cb70
Isabella Kite <isabella.kite@soulmachines.com>
2022-06-09 15:18:06 +1200

    Add tailwind styling for button

--- rebuild/src/components/ButtonIcon/ButtonIcon.tsx
+++ rebuild/src/components/ButtonIcon/ButtonIcon.tsx
@@ -1,3 +1,7 @@
+import path from "./paths";
+
 export function ButtonIcon() {
-  return <p>Hello world</p>;
+  return (<div key={path} className="sm-flex sm-flex-row sm-justify-center sm-items-center sm-px-4 sm-py-2 sm-absolute sm-bg-primary-600 sm-rounded-lg">
+    <span className="sm-font-rubik sm-text-center sm-text-sm sm-not-italic sm-tracking-wider sm-font-medium sm-text-white sm-flex-none sm-flex-grow-0">BUTTON</span>
+    </div>);
 }
