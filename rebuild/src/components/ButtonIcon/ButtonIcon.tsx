bd26d95c29d83f39b3b0e46c054e92cfc36a6206
Isabella Kite <isabella.kite@soulmachines.com>
2022-06-10 15:37:33 +1200

    add sm-cursor-pointer to styling

--- rebuild/src/components/ButtonIcon/ButtonIcon.tsx
+++ rebuild/src/components/ButtonIcon/ButtonIcon.tsx
@@ -5,7 +5,7 @@ export type ButtonOptions = {
 };
 
 export function ButtonControls({title}: ButtonOptions) {
-  return (<div key={path} className="sm-flex sm-flex-row sm-justify-center sm-items-center sm-px-4 sm-py-2 sm-absolute sm-bg-primary-600 sm-rounded-lg hover:sm-bg-primary-700 active:sm-bg-primary-800 focus:sm-bg-primary-600 focus:sm-border-x-blue-500 focus:sm-drop-shadow-lg disabled:sm-bg-primary-200">
+  return (<div key={path} className="sm-cursor-pointer sm-flex sm-flex-row sm-justify-center sm-items-center sm-px-4 sm-py-2 sm-absolute sm-bg-primary-600 sm-rounded-lg hover:sm-bg-primary-700 active:sm-bg-primary-800 focus:sm-bg-primary-600 focus:sm-border-x-blue-500 focus:sm-drop-shadow-lg disabled:sm-bg-primary-200">
     <span className="sm-font-rubik sm-text-center sm-text-sm sm-not-italic sm-tracking-wider sm-font-medium sm-text-white sm-flex-none sm-flex-grow-0">{title}</span>
     </div>);
 }
