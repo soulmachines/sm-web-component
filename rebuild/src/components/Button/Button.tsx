9a6b143dc5966a76fe43c288ca4560a2fbe5bbd1
Isabella Kite <isabella.kite@soulmachines.com>
2022-06-10 16:34:14 +1200

    change to use button tag instead of div

--- rebuild/src/components/Button/Button.tsx
+++ rebuild/src/components/Button/Button.tsx
@@ -3,5 +3,5 @@ export type ButtonProps = {
 };
 
 export function Button({title}: ButtonProps) {
-  return (<div className="sm-cursor-pointer sm-justify-center sm-items-center sm-px-4 sm-py-2 sm-absolute sm-bg-primary-600 sm-rounded-lg hover:sm-bg-primary-700 active:sm-bg-primary-800 focus:sm-bg-primary-600 focus:sm-border-x-blue-500 focus:sm-drop-shadow-lg disabled:sm-bg-primary-200 sm-font-rubik sm-text-center sm-text-sm sm-tracking-wider sm-font-medium sm-text-white">{title}</div>);
+  return (<button className="sm-cursor-pointer sm-justify-center sm-items-center sm-px-4 sm-py-2 sm-absolute sm-bg-primary-600 sm-rounded-lg hover:sm-bg-primary-700 active:sm-bg-primary-800 focus:sm-bg-primary-600 focus:sm-border-x-blue-500 focus:sm-drop-shadow-lg disabled:sm-bg-primary-200 sm-font-rubik sm-text-center sm-text-sm sm-tracking-wider sm-uppercase sm-font-medium sm-text-white">{title}</button>);
 }
