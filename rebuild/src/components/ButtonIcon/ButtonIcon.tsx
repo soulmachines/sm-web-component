b2eb3011f11f18da7852c5adb0b0abc0411940e5
Isabella Kite <isabella.kite@soulmachines.com>
2022-06-10 15:43:11 +1200

    remove unneeded path to svg

--- rebuild/src/components/ButtonIcon/ButtonIcon.tsx
+++ rebuild/src/components/ButtonIcon/ButtonIcon.tsx
@@ -1,9 +1,7 @@
-import path from "./paths";
-
 export type ButtonOptions = {
   title: string
 };
 
 export function ButtonControls({title}: ButtonOptions) {
-  return (<div key={path} className="sm-cursor-pointer sm-flex sm-flex-row sm-justify-center sm-items-center sm-px-4 sm-py-2 sm-absolute sm-bg-primary-600 sm-rounded-lg hover:sm-bg-primary-700 active:sm-bg-primary-800 focus:sm-bg-primary-600 focus:sm-border-x-blue-500 focus:sm-drop-shadow-lg disabled:sm-bg-primary-200 sm-font-rubik sm-text-center sm-text-sm sm-not-italic sm-tracking-wider sm-font-medium sm-text-white sm-flex-none sm-flex-grow-0">{title}</div>);
+  return (<div className="sm-cursor-pointer sm-flex sm-flex-row sm-justify-center sm-items-center sm-px-4 sm-py-2 sm-absolute sm-bg-primary-600 sm-rounded-lg hover:sm-bg-primary-700 active:sm-bg-primary-800 focus:sm-bg-primary-600 focus:sm-border-x-blue-500 focus:sm-drop-shadow-lg disabled:sm-bg-primary-200 sm-font-rubik sm-text-center sm-text-sm sm-not-italic sm-tracking-wider sm-font-medium sm-text-white sm-flex-none sm-flex-grow-0">{title}</div>);
 }
