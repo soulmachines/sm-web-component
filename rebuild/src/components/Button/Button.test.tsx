96447759c4f8e3e96c137d80c908f6f6ca13de0e
Isabella Kite <isabella.kite@soulmachines.com>
2022-06-13 10:42:09 +1200

    add test for button component

--- rebuild/src/components/Button/Button.test.tsx
+++ rebuild/src/components/Button/Button.test.tsx
@@ -1,5 +1,9 @@
+import { render } from '@testing-library/preact';
 import { Button } from '.';
 
 describe('<Button />', () => {
-
+    it('render button and verify text appears in document', () => {
+        const { getByText } = render(<Button title="button" />);
+        expect(getByText('button')).toBeInTheDocument();
+    });
 });
