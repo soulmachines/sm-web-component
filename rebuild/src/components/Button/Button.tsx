2c921722054f88cf4500f33078302d4aa09044ec
Isabella Kite <isabella.kite@soulmachines.com>
2022-06-13 14:46:53 +1200

    change box border to blue

--- rebuild/src/components/Button/Button.tsx
+++ rebuild/src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ export type ButtonProps = {
 
 export function Button({ children }: ButtonProps) {
   return (
-    <button className="sm-cursor-pointer sm-px-4 sm-py-2 sm-font-rubik sm-text-sm sm-tracking-wider sm-uppercase sm-font-medium sm-text-white sm-bg-primary-600 sm-rounded-lg hover:sm-bg-primary-700 active:sm-bg-primary-800 focus:sm-bg-primary-600 focus:sm-border-x-blue-500 disabled:sm-bg-primary-200">
+    <button className="sm-cursor-pointer sm-px-4 sm-py-2 sm-font-rubik sm-text-sm sm-tracking-wider sm-uppercase sm-font-medium sm-text-white sm-bg-primary-600 sm-rounded-lg hover:sm-bg-primary-700 active:sm-bg-primary-800 focus:sm-bg-primary-600 focus:sm-border-x-blue-500 focus:sm-border-blue-300 disabled:sm-bg-primary-200">
       {children}
     </button>
   );
