5e7b9108ad938da7614273035d6ec00e7602cce5
Isabella Kite <isabella.kite@soulmachines.com>
2022-06-13 12:11:51 +1200

    feat: create button with tailwind styling

---
+++ rebuild/src/components/Button/Button.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/preact';
+import { Button } from '.';
+
+describe('<Button />', () => {
+    it('render button and verify text appears in document', () => {
+        const { getByText } = render(<Button>button</Button>);
+        expect(getByText('button')).toBeInTheDocument();
+    });
+});
---
+++ rebuild/src/components/Button/Button.tsx
@@ -0,0 +1,7 @@
+export type ButtonProps = {
+  children: string
+};
+
+export function Button({children}: ButtonProps) {
+  return (<button className="sm-cursor-pointer sm-justify-center sm-items-center sm-px-4 sm-py-2 sm-absolute sm-bg-primary-600 sm-rounded-lg hover:sm-bg-primary-700 active:sm-bg-primary-800 focus:sm-bg-primary-600 focus:sm-border-x-blue-500 focus:sm-drop-shadow-lg disabled:sm-bg-primary-200 sm-font-rubik sm-text-center sm-text-sm sm-tracking-wider sm-uppercase sm-font-medium sm-text-white">{children}</button>);
+}
