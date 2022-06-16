65034a7414e3699c89d3cb5cfb0c5970c31f2002
Allan Pope <allan.pope@soulmachines.com>
2022-06-16 15:31:28 +1200

    fix: card so close icon is visible

--- rebuild/src/components/Card/Card.tsx
+++ rebuild/src/components/Card/Card.tsx
@@ -17,15 +17,17 @@ export function Card({ children, isDismissible, style }: CardProps = { isDismiss
 
   return (
     <div
-      className="sm-bg-white sm-rounded-xl sm-relative sm-w-full sm-px-6 sm-py-4 sm-shadow-lg sm-overflow-y-auto sm-pointer-events-auto"
+      className="sm-relative sm-flex sm-overflow-hidden sm-pointer-events-auto sm-p-8 -sm-m-8"
       style={style}
     >
-      {children}
+      <div className="sm-bg-white sm-rounded-xl sm-w-full sm-px-6 sm-py-4 sm-shadow-lg sm-overflow-y-auto">
+        {children}
+      </div>
 
       {isDismissible && (
         <button
           onClick={() => setIsHidden(true)}
-          className="sm-absolute sm-top-0 sm-right-0 sm-translate-x-1/2 -sm-translate-y-2/4"
+          className="sm-absolute sm-top-8 sm-right-8 sm-translate-x-1/2 -sm-translate-y-2/4"
         >
           <Icon name="close" title="Hide card" />
         </button>
