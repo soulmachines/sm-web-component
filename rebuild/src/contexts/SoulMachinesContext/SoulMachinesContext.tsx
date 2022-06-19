1a8500e56870873115ec8f7959487f8afcc1069a
Allan Pope <allan.pope@soulmachines.com>
2022-06-20 10:36:52 +1200

    fix: wrap in try/catch since its a promise

--- rebuild/src/contexts/SoulMachinesContext/SoulMachinesContext.tsx
+++ rebuild/src/contexts/SoulMachinesContext/SoulMachinesContext.tsx
@@ -38,7 +38,11 @@ function SoulMachinesProvider({ children, apiKey, tokenServer }: SoulMachinesPro
   );
   const persona = new Persona(scene, personaId);
   const sendTextMessage = (text: string) => {
-    persona.conversationSend(text, {}, {});
+    try {
+      persona.conversationSend(text, {}, {});
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const { videoRef, connect, disconnect, connectionStatus, connectionError } = useConnection(
