6324359491fed0806be4146e5b5b7db1d95cb15b
Allan Pope <allan.pope@soulmachines.com>
2022-06-20 14:53:09 +1200

    feat: create theme for button

--- rebuild/src/components/Button/Button.tsx
+++ rebuild/src/components/Button/Button.tsx
@@ -1,11 +1,21 @@
+import classNames from 'classnames';
+
 export type ButtonProps = {
   children: string;
+  theme: 'default' | 'outline';
 };
 
-export function Button({ children }: ButtonProps) {
-  return (
-    <button className="sm-cursor-pointer sm-px-4 sm-py-2 sm-font-rubik sm-text-base sm-tracking-wider sm-uppercase sm-font-medium sm-text-white sm-bg-primary-600 sm-rounded-lg hover:sm-bg-primary-700 active:sm-bg-primary-800 focus:sm-bg-primary-600 focus:sm-outline focus:sm-outline-2 focus:sm-outline-primary-200 disabled:sm-bg-primary-200 disabled:sm-cursor-not-allowed">
-      {children}
-    </button>
-  );
+export function Button({ children, theme }: ButtonProps) {
+  const buttonClass = classNames({
+    'sm-cursor-pointer sm-px-4 sm-py-2 sm-font-rubik sm-rounded-lg sm-text-base sm-tracking-wider sm-uppercase sm-font-medium  disabled:sm-bg-primary-200 focus:sm-outline focus:sm-outline-2 focus:sm-outline-primary-200 disabled:sm-cursor-not-allowed':
+      true,
+    'sm-text-white sm-bg-primary-600 hover:sm-bg-primary-700 active:sm-bg-primary-800 focus:sm-bg-primary-600':
+      theme === 'default',
+    '': theme === 'outline',
+  });
+
+  return <button className={buttonClass}>{children}</button>;
 }
+Button.defaultProps = {
+  theme: 'default',
+};
