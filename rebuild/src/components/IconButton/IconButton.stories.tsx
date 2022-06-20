5984c5aa41a6ec9c4b1a74ba10fc5394afa53b83
Isabella Kite <isabella.kite@soulmachines.com>
2022-06-20 16:51:30 +1200

    rename enum StyleType to Theme

--- rebuild/src/components/IconButton/IconButton.stories.tsx
+++ rebuild/src/components/IconButton/IconButton.stories.tsx
@@ -1,24 +1,24 @@
 import { IconButton } from '.';
 import paths from '../Icon/paths';
-import { IconButtonProps, StyleType } from './IconButton';
+import { IconButtonProps, Theme } from './IconButton';
 
 export default {
   title: `Components / IconButton`,
   component: IconButton,
 };
 
-export const Basic = ({ styleType, shadow, size }: IconButtonProps) => <div>
+export const Basic = ({ theme, shadow, size }: IconButtonProps) => <div>
   {Object.keys(paths).map((key) => {
   return (
     <div>
-      <IconButton name={key} size={size} title={key} shadow={shadow} styleType={styleType}/>
+      <IconButton name={key} size={size} title={key} shadow={shadow} theme={theme}/>
     </div>
   );
   })}
   </div>
 
 Basic.args = {
-  styleType: StyleType.default,
+  theme: Theme.default,
   shadow: false,
   size: 40
 }
--- rebuild/src/components/IconButton/IconButton.tsx
+++ rebuild/src/components/IconButton/IconButton.tsx
@@ -2,20 +2,20 @@ import classNames from "classnames";
 import { Icon } from "../Icon";
 import paths from "../Icon/paths";
 
-export enum StyleType { default, danger };
+export enum Theme { default, danger };
 
 export type IconButtonProps = {
   name: keyof typeof paths,
   size?: number,
   title: string,
   shadow?: boolean,
-  styleType: StyleType
+  theme: Theme
 };
 
-export function IconButton( { name, size, title, shadow, styleType }: IconButtonProps) {
+export function IconButton( { name, size, title, shadow, theme }: IconButtonProps) {
   const buttonClass = classNames({
-    "sm-bg-white sm-rounded-full sm-p-3 focus:sm-outline focus:sm-outline-2 focus:sm-outline-primary-200" : styleType == StyleType.default,
-    "sm-bg-error-400 sm-rounded-full sm-p-3 sm-text-white hover:sm-bg-error-500 focus:sm-outline focus:sm-outline-2 focus:sm-outline-primary-200" : styleType == StyleType.danger,
+    "sm-bg-white sm-rounded-full sm-p-3 focus:sm-outline focus:sm-outline-2 focus:sm-outline-primary-200" : theme == Theme.default,
+    "sm-bg-error-400 sm-rounded-full sm-p-3 sm-text-white hover:sm-bg-error-500 focus:sm-outline focus:sm-outline-2 focus:sm-outline-primary-200" : theme == Theme.danger,
     "sm-shadow": shadow
   });
   return (
