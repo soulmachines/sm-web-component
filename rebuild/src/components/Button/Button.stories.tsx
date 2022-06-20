115b08f53c57f804cb087a9b8c59af9022bbd0f1
Allan Pope <allan.pope@soulmachines.com>
2022-06-20 14:53:09 +1200

    feat: create theme for button

--- rebuild/src/components/Button/Button.stories.tsx
+++ rebuild/src/components/Button/Button.stories.tsx
@@ -3,10 +3,15 @@ import { Button, ButtonProps } from '.';
 export default {
   title: `Components / Button`,
   component: Button,
+  argTypes: {
+    theme: { control: 'select', options: ['default', 'outline'], defaultValue: 'outline' },
+    children: {
+      type: { name: 'string' },
+      defaultValue: 'Hello',
+    },
+  },
 };
 
-export const Basic = ({ children }: ButtonProps) => <Button children={children} />;
-
-Basic.args = {
-  children: 'button',
-};
+export const Basic = ({ children, theme }: ButtonProps) => (
+  <Button theme={theme} children={children} />
+);
