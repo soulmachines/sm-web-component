1f73f983be0fb4b25539500e073cbc664fa4f1a3
Allan Pope <allan.pope@soulmachines.com>
2022-06-20 14:38:11 +1200

    chore: update dependencies quic-1834 (#225)
    * chore: upgrade cypress and run migration
    
    * chore: update sorybook
    
    * chore: update minor deps
    
    * chore: update deps
    
    * chore: update lock file
    
    * fix: update config names
    
    * fix: run prettier
    
    * refactor: use ts file
    
    * fix: point to e2e folder
    
    * fix: undo rename
    
    * fix: increase timeout for cc

---
+++ rebuild/cypress.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  retries: 2,
+  video: false,
+  projectId: 'uabajg',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.ts')(on, config);
+    },
+    baseUrl: 'http://localhost:5050/',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+});
