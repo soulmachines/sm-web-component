461d1a0fc970e016863f88e6e8c0772984a2b4b6
Isabella Kite <isabella.kite@soulmachines.com>
2022-06-20 15:16:45 +1200

    add basic unit test for default svg

--- rebuild/src/components/IconButton/IconButton.test.tsx
+++ rebuild/src/components/IconButton/IconButton.test.tsx
@@ -1,5 +1,11 @@
-import { IconButton } from '.';
+import { render } from '@testing-library/preact';
+import { IconButton, StyleType } from '.';
 
 describe('<IconButton />', () => {
+    it('render button and verify it appears in document', () => {
+        const { container } = render(<IconButton name={'microphone'} styleType={StyleType.default}/>);
+        const svg = container.querySelector('svg');
 
+        expect(svg).not.toBeNull();
+    });
 });
