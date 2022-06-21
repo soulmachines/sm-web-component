274baf4c5437d87b040e88d67147dd666baaa073
Isabella Kite <isabella.kite@soulmachines.com>
2022-06-21 14:29:44 +1200

    add unit test for onclick

--- rebuild/src/components/IconButton/IconButton.test.tsx
+++ rebuild/src/components/IconButton/IconButton.test.tsx
@@ -6,4 +6,15 @@ describe('<IconButton />', () => {
         const { queryByTitle } = render(<IconButton name={'microphone'} title={"microphone"}/>);
         expect(queryByTitle('microphone')).toBeInTheDocument();
     });
+
+    it('passes the onClick function to the button', async () => {
+        const mock = jest.fn();
+        const { queryByTitle } = render(<IconButton name={'microphone'} title={'microphone'} onClick={mock}/>);
+        const button = queryByTitle('microphone');
+        expect(button).not.toBeNull();
+        if (button) {
+            await fireEvent.click(button);
+            expect(mock).toHaveBeenCalled();
+        }
+    });
 });
