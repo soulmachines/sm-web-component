1faa1bf44e08756622a190c4728aca205ced85ec
Isabella Kite <isabella.kite@soulmachines.com>
2022-06-21 15:51:34 +1200

    add prettier fixes

--- rebuild/src/components/IconButton/IconButton.stories.tsx
+++ rebuild/src/components/IconButton/IconButton.stories.tsx
@@ -5,14 +5,14 @@ import { IconButtonProps } from './IconButton';
 export default {
   title: `Components / IconButton`,
   component: IconButton,
-  argTypes: { 
-    theme: {  control: 'radio', options: ['default', 'danger'], defaultValue: 'default' }, 
-    shadow: {  control: 'boolean' , defaultValue: true},
-    size: { control: 'number', defaultValue: 40 },
-    name: { control: 'radio', options: Object.keys(paths), defaultValue: 'microphone' }
-  }
+  argTypes: {
+    theme: { control: 'radio', options: ['default', 'danger'], defaultValue: 'default' },
+    shadow: { control: 'boolean', defaultValue: true },
+    size: { control: 'number', defaultValue: 20 },
+    name: { control: 'radio', options: Object.keys(paths), defaultValue: 'microphone' },
+  },
 };
 
 export const Basic = ({ theme, shadow, size, name }: IconButtonProps) => (
-  <IconButton name={name} size={size} title={name} shadow={shadow} theme={theme}/>
-)
+  <IconButton name={name} size={size} title={name} shadow={shadow} theme={theme} />
+);
