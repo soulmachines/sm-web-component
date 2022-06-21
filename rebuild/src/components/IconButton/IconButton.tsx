93b9ca16841bf6fe2e146b4f7ca600c5c370b62c
Isabella Kite <isabella.kite@soulmachines.com>
2022-06-21 15:51:34 +1200

    add prettier fixes

--- rebuild/src/components/IconButton/IconButton.tsx
+++ rebuild/src/components/IconButton/IconButton.tsx
@@ -1,30 +1,32 @@
-import classNames from "classnames";
-import { Icon } from "../Icon";
-import paths from "../Icon/paths";
+import classNames from 'classnames';
+import { Icon } from '../Icon';
+import paths from '../Icon/paths';
 
-export enum Theme { 
-  default = 'default', 
+export enum Theme {
+  default = 'default',
   danger = 'danger',
 }
 
 export type IconButtonProps = {
-  name: keyof typeof paths,
-  size?: number,
-  title: string,
-  shadow?: boolean,
-  theme: Theme,
-  onClick?: () => void
-}
+  name: keyof typeof paths;
+  size?: number;
+  title: string;
+  shadow?: boolean;
+  theme: Theme;
+  onClick?: () => void;
+};
 
-export function IconButton( { name, size, title, shadow, theme, onClick }: IconButtonProps) {
+export function IconButton({ name, size, title, shadow, theme, onClick }: IconButtonProps) {
   const buttonClass = classNames({
-    "sm-bg-white sm-rounded-full sm-p-3 focus:sm-outline focus:sm-outline-2 focus:sm-outline-primary-200" : theme == Theme.default,
-    "sm-bg-error-400 sm-rounded-full sm-p-3 sm-text-white hover:sm-bg-error-500 focus:sm-outline focus:sm-outline-2 focus:sm-outline-primary-200" : theme == Theme.danger,
-    "sm-shadow": shadow
+    'sm-bg-white sm-rounded-full sm-p-3 focus:sm-outline focus:sm-outline-2 focus:sm-outline-primary-200':
+      theme == Theme.default,
+    'sm-bg-error-400 sm-rounded-full sm-p-3 sm-text-white hover:sm-bg-error-500 focus:sm-outline focus:sm-outline-2 focus:sm-outline-primary-200':
+      theme == Theme.danger,
+    'sm-shadow': shadow,
   });
   return (
     <button onClick={onClick} className={buttonClass}>
-        <Icon name={name} size={size} title={title}/>
+      <Icon name={name} size={size} title={title} />
     </button>
   );
 }
@@ -32,4 +34,4 @@ export function IconButton( { name, size, title, shadow, theme, onClick }: IconB
 IconButton.defaultProps = {
   theme: Theme.default,
   size: '1.375rem', // rem for 22px.
- }
+};
