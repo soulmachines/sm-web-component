c60f79218fc022c0c8c05dda8e4b7ad3b3cf360b
Allan Pope <allan.pope@soulmachines.com>
2022-06-21 14:24:12 +1200

    feat: shrink card gaps on mobile

--- rebuild/src/components/Widget/Widget.tsx
+++ rebuild/src/components/Widget/Widget.tsx
@@ -44,8 +44,8 @@ export function Widget({ profilePicture, greeting, loadingIndicator }: WidgetPro
 
   return (
     <div className="sm-fixed sm-bottom-0 sm-right-0 sm-p-2 sm-z-max sm-pointer-events-none sm-h-full md:sm-p-5 ">
-      <div className="sm-flex sm-flex-col sm-items-end sm-gap-y-5 sm-h-full sm-justify-end">
-        <div class="sm-w-63 md:sm-w-88 sm-max-h-full sm-flex sm-flex-col sm-justify-end sm-gap-y-3 sm-overflow-hidden sm-p-8 -sm-m-8 sm-box-content">
+      <div className="sm-flex sm-flex-col sm-items-end sm-gap-y-2 sm-h-full sm-justify-end md:sm-gap-y-5">
+        <div class="sm-w-63 md:sm-w-88 sm-max-h-full sm-flex sm-flex-col sm-justify-end sm-gap-y-2 sm-overflow-hidden sm-p-8 -sm-m-8 sm-box-content md:sm-gap-y-3">
           <ContentCards />
         </div>
 
