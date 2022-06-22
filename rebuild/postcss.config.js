7fe87ad0b0259107de33ed2623061df4f3854747
Allan Pope <allan.pope@soulmachines.com>
2022-06-22 12:34:21 +1200

    feat: convert rem to px

--- rebuild/postcss.config.js
+++ rebuild/postcss.config.js
@@ -1,13 +1,15 @@
-const productionPlugins = {
-  cssnano: {
-    preset: ['default', { discardComments: { removeAll: true } }],
-  },
-};
+const productionPlugins = [
+  require('cssnano')({
+    preset: 'default',
+    discardComments: { removeAll: true },
+  }),
+];
 
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? productionPlugins : {}),
-  },
+  plugins: [
+    require('tailwindcss'),
+    require('@thedutchcoder/postcss-rem-to-px', { baseValue: 10 }),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? productionPlugins : []),
+  ],
 };
