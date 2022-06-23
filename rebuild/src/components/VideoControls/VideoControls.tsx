a87d477594827658adfeca38c462af52090bf55b
Isabella Kite <isabella.kite@soulmachines.com>
2022-06-23 13:31:05 +1200

    replace video control buttons with IconButton components

--- rebuild/src/components/VideoControls/VideoControls.tsx
+++ rebuild/src/components/VideoControls/VideoControls.tsx
@@ -1,6 +1,7 @@
 import { useSoulMachines } from '../../contexts/SoulMachinesContext';
 import { Icon } from '../Icon';
 import { useSMMedia } from '../../hooks/useSMMedia';
+import { IconButton } from '../IconButton';
 
 export function VideoControls() {
   const { scene, disconnect } = useSoulMachines();
@@ -14,20 +15,13 @@ export function VideoControls() {
 
   return (
     <div className="sm-p-3 sm-flex sm-flex-col sm-justify-between sm-absolute sm-top-0 sm-left-0 sm-w-full sm-h-full">
-      <div className="sm-flex sm-justify-between">
-        <button onClick={disconnect} className="sm-ml-auto">
-          <Icon name="chevronDown" title="Close video" />
-        </button>
+      <div className="sm-flex sm-justify-between sm-ml-auto">
+        <IconButton onClick={disconnect} name="chevronDown" title="Close video"/>
       </div>
 
       <div className="sm-flex sm-justify-between">
-        <button onClick={toggleMicrophone}>
-          <Icon name={microphoneIcon} title={microphoneText} />
-        </button>
-
-        <button onClick={toggleCamera}>
-          <Icon name={cameraIcon} title={cameraText} />
-        </button>
+        <IconButton onClick={toggleMicrophone} name={microphoneIcon} title={microphoneText} />
+        <IconButton onClick={toggleCamera} name={cameraIcon} title={cameraText} />
       </div>
     </div>
   );
