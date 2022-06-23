0056a1367a3e2f13706979cd7814e66e90b56860
Isabella Kite <isabella.kite@soulmachines.com>
2022-06-23 13:18:00 +1200

    add flexbox spacing

--- rebuild/src/components/Notifications/Notifications.tsx
+++ rebuild/src/components/Notifications/Notifications.tsx
@@ -15,19 +15,19 @@ export function Notifications({ greeting }: NotificationsProps) {
   const renderContent = () => {
     if (connectionStatus === ConnectionStatus.ERRORED) {
       return (
-        <Fragment>
+        <div className="sm-flex sm-flex-col sm-gap-y-3 sm-items-start">
           <Text>{`Unable to connect. ${connectionError?.message}`}</Text>
           <Button onClick={connect}>Retry</Button>
-        </Fragment>
+        </div>
       );
     }
 
     if (connectionStatus === ConnectionStatus.TIMED_OUT) {
       return (
-        <Fragment>
+        <div className="sm-flex sm-flex-col sm-gap-y-3 sm-items-start">
           <Text>Your session has ended. You can reconnect anytime you are ready.</Text>
           <Button onClick={connect}>Connect</Button>
-        </Fragment>
+        </div>
       );
     }
 
