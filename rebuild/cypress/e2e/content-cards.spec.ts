a2337951e0a6c056c20ddf84ca9e73ec0a66b841
Allan Pope <allan.pope@soulmachines.com>
2022-06-27 16:03:01 +1200

    refactor: split up tests into multiple files

---
+++ rebuild/cypress/e2e/content-cards.spec.ts
@@ -0,0 +1,17 @@
+describe('content cards', () => {
+  const corpusCommands = {
+    optionsCard: '47',
+  };
+
+  before(() => {
+    cy.visit('/');
+    cy.launchScene();
+  });
+
+  it('renders an options content block and the hides options block once its interacted with', () => {
+    cy.sendTextMessage(corpusCommands.optionsCard);
+    cy.get('[data-sm-content=options]', { timeout: 5000 }).should('exist');
+    cy.get('button').contains('Tell me a joke').should('exist').click();
+    cy.get('[data-sm-content=options]').should('not.exist');
+  });
+});
---
+++ rebuild/cypress/e2e/scene-disconnected.ts
@@ -0,0 +1,23 @@
+describe('scene disconnected', () => {
+  before(() => {
+    cy.visit('/');
+  });
+
+  it('renders a greeting', () => {
+    cy.get('[data-sm-cy=greetingText]').should('be.visible');
+  });
+
+  it('does not render a camera button', () => {
+    cy.get('title').contains('Disable camera').should('not.exist');
+    cy.get('title').contains('Enable camera').should('not.exist');
+  });
+
+  it('does not render a microphone button', () => {
+    cy.get('title').contains('Disable microphone').should('not.exist');
+    cy.get('title').contains('Enable microphone').should('not.exist');
+  });
+
+  it('renderes a profile picture', () => {
+    cy.get('[data-sm-cy=profileImage]').should('be.visible');
+  });
+});
