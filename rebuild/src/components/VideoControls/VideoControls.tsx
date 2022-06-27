1c64bc73e83ac5dea116360dd2baa3f55f2f75b1
Allan Pope <allan.pope@soulmachines.com>
2022-06-27 13:14:10 +1200

    feat: add mute button

--- rebuild/src/components/VideoControls/VideoControls.tsx
+++ rebuild/src/components/VideoControls/VideoControls.tsx
@@ -1,12 +1,19 @@
 import { useSoulMachines } from '../../contexts/SoulMachinesContext';
-import { useSMMedia } from '../../hooks/useSMMedia';
 import { IconButton, Theme } from '../IconButton';
 
 export function VideoControls() {
-  const { scene, disconnect } = useSoulMachines();
-  const { isMicrophoneEnabled, isCameraEnabled, toggleMicrophone, toggleCamera } =
-    useSMMedia(scene);
+  const {
+    disconnect,
+    isMicrophoneEnabled,
+    isCameraEnabled,
+    isVideoMuted,
+    toggleMicrophone,
+    toggleCamera,
+    toggleVideoMuted,
+  } = useSoulMachines();
 
+  const muteIcon = isVideoMuted ? 'volumeOff' : 'volume';
+  const muteText = isVideoMuted ? 'Unmute video' : 'Mute video';
   const microphoneIcon = isMicrophoneEnabled ? 'microphone' : 'microphoneOff';
   const cameraIcon = isCameraEnabled ? 'camera' : 'cameraOff';
   const microphoneText = isMicrophoneEnabled ? 'Disable microphone' : 'Enable microphone';
@@ -14,7 +21,8 @@ export function VideoControls() {
 
   return (
     <div className="sm-p-3 sm-flex sm-flex-col sm-justify-between sm-absolute sm-top-0 sm-left-0 sm-w-full sm-h-full">
-      <div className="sm-flex sm-justify-between sm-ml-auto">
+      <div className="sm-flex sm-justify-between">
+        <IconButton onClick={toggleVideoMuted} name={muteIcon} title={muteText} />
         <IconButton onClick={disconnect} name="chevronDown" title="Close video" />
       </div>
 
