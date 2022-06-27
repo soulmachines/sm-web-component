0de3229e1c541727f9821b80c9d97cda32b380c0
Allan Pope <allan.pope@soulmachines.com>
2022-06-27 15:18:17 +1200

    tests: add cypress tests for when an error occurs  quic-1800 (#239)

--- rebuild/cypress/e2e/ui-tests.spec.ts
+++ rebuild/cypress/e2e/ui-tests.spec.ts
@@ -53,4 +53,27 @@ describe('scene', () => {
       cy.get('[data-sm-cy=profileImage]').should('be.visible');
     });
   });
+
+  describe('when an error occurs', () => {
+    before(() => {
+      cy.visit('/invalid-api-key.html');
+      cy.get('[data-sm-cy=connectButton]').should('be.visible').click();
+    });
+
+    it('displays the error along with the error message', () => {
+      cy.contains('Unable to connect. Invalid API key');
+    });
+
+    it('renders a retry button', () => {
+      cy.contains('button', 'Retry');
+    });
+
+    it('does not render a video', () => {
+      cy.get('video').should('not.exist');
+    });
+
+    it('does not render a greeting', () => {
+      cy.get('[data-sm-cy=greetingText]').should('not.exist');
+    });
+  });
 });
