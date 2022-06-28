264ee5ad7686cdbe43366fe9f84b01691d5ed289
Allan Pope <allan.pope@soulmachines.com>
2022-06-29 11:31:38 +1200

    chore: remove angular

--- src/app/types/boolstring.type.ts
+++
@@ -1,9 +0,0 @@
-export type boolstring = 'true' | 'false';
-
-export function convertToBool(boolString: boolstring): boolean {
-  return boolString === 'true';
-}
-
-export function convertToBoolString(bool: boolean): boolstring {
-  return bool ? 'true' : 'false';
-}
--- src/app/types/theme.type.ts
+++
@@ -1,2 +0,0 @@
-export const themes = ['default', 'none'] as const;
-export type Theme = typeof themes[number];
