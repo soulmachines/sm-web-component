6cc888b06bd7926075195d8c603ba5677ab9d1f6
Isabella Kite <isabella.kite@soulmachines.com>
2022-07-01 09:52:27 +1200

    change reopener to referrer

--- src/components/LinkCard/LinkCard.tsx
+++ src/components/LinkCard/LinkCard.tsx
@@ -23,7 +23,7 @@ export function LinkCard({ content, style }: LinkCardProps) {
       <div data-sm-content={content.id} className="sm-flex sm-flex-col sm-gap-y-3 sm-items-start">
         <Heading type="h2">{data.title}</Heading>
         <Text>{data.description}</Text>
-        <a className="sm-text-white sm-no-underline" href={data.url} target="_blank" rel="noopener">
+        <a className="sm-text-white sm-no-underline" href={data.url} target="_blank" rel="noreferrer">
           <Button>View Page</Button>
         </a>
       </div>
