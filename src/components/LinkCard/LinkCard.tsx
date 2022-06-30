9174f5d191a370fe408fbc84f9ac55e13a312ecb
Isabella Kite <isabella.kite@soulmachines.com>
2022-07-01 09:17:48 +1200

    add heading

--- src/components/LinkCard/LinkCard.tsx
+++ src/components/LinkCard/LinkCard.tsx
@@ -2,6 +2,7 @@ import { Button } from '../Button';
 import { Card } from '../Card';
 import { Text } from '../Text';
 import { ContentCard } from '@soulmachines/smwebsdk';
+import { Heading } from '../Heading';
 
 export type LinkCardProps = {
   content: ContentCard;
@@ -12,7 +13,7 @@ export type LinkData = {
   id: string;
   url: string;
   title: string;
-  description?: string;
+  description: string;
 };
 
 export function LinkCard({ content, style }: LinkCardProps) {
@@ -20,10 +21,11 @@ export function LinkCard({ content, style }: LinkCardProps) {
   return (
     <Card style={style}>
       <div data-sm-content={content.id} className="sm-flex sm-flex-col sm-gap-y-3 sm-items-start">
+        <Heading type="h2">{data.title}</Heading>
         <Text>{data.description}</Text>
         <Button>
           <a className="sm-text-white sm-no-underline" href={data.url} target="_blank">
-            {data.title}
+            View Page
           </a>
         </Button>
       </div>
