f17c715eed18afc721ec5661fca38dbbd7be90ed
isabella-k <66455891+isabella-k@users.noreply.github.com>
2022-07-12 16:44:23 +1200

    test: add cypress test for internal link card (#267)

--- cypress/e2e/content-cards.spec.ts
+++ cypress/e2e/content-cards.spec.ts
@@ -3,6 +3,7 @@ describe('content cards', () => {
     optionsCard: '47',
     imageCard: '46',
     externalLink: '48',
+    internalLink: '48c',
   };
 
   before(() => {
@@ -24,10 +25,17 @@ describe('content cards', () => {
     cy.get('[data-sm-content=image]').should('not.exist');
   });
 
-  it('renders an link card and hides the card once the close button is clicked', () => {
+  it('renders an external link card and hides the card once the close button is clicked', () => {
     cy.sendTextMessage(corpusCommands.externalLink);
     cy.get('[data-sm-content=externalLink]', { timeout: 6000 }).should('exist');
     cy.get('button').contains('svg', 'Hide card').click();
     cy.get('[data-sm-content=externalLink]').should('not.exist');
+  });
+
+  it('renders an internal link card and hides the card once the close button is clicked', () => {
+    cy.sendTextMessage(corpusCommands.internalLink);
+    cy.get('[data-sm-content=internalLink]', { timeout: 6000 }).should('exist');
+    cy.get('button').contains('svg', 'Hide card').click();
+    cy.get('[data-sm-content=internalLink]').should('not.exist');
   });
 });
