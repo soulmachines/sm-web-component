23ff243df450d5b28a873a4a2b1516761c9e2e00
John Niedfeldt <john.niedfeldt@soulmachines.com>
2022-07-14 13:41:16 -0700

    Updated structure to make it easier to add Markdown. There is some issue with empty lines/space around lists and sublists not rendering as anticipated.

--- src/components/MarkdownCard/MarkdownCard.stories.tsx
+++ src/components/MarkdownCard/MarkdownCard.stories.tsx
@@ -4,17 +4,43 @@ const content = {
   id: 'id',
   type: 'markdown',
   data: {
-    text: '# H1\r\n## H2\r\n### H3\r\n#### H4\r\n##### H5\r\n###### H6\r\n 1. Most interesting\r\n 2. Less interesting\r\n 3. Least Interesting\r\n\r\n**bold**\r\n- item 1\r\n- item 2\r\n\r\n[External Link](https://www.google.com)\r\n\r\n[Internal Link](http://localhost:3000)\r\n\r\n*emphasis*\r\n\r\n---\r\nHorizontal Rule\r\n\r\n---\r\n\r\n- [ ] checkbox 1\r\n- [ ] checkbox 2\r\n- [ ] checkbox 3\r\n  - [ ] sub checkbox\r\n',
+    text: `# H1
+## H2
+### H3
+#### H4
+##### H5
+###### H6
+
+1. Most interesting
+   1. Sublist 1
+   2. Sublist 2
+2. Less interesting
+   1. Sublist 1
+   2. Sublist 2
+3. Least Interesting
+
+**bold**
+- item 1
+- item 2
+
+[External Link](https://www.google.com)
+
+[Internal Link](http://localhost:3000)
+
+*emphasis*
+
+---
+Horizontal Rule
+
+---
+
+- [ ] checkbox 1
+- [ ] checkbox 2
+- [ ] checkbox 3
+  - [ ] sub checkbox
+`,
   },
 };
-
-// const exhaustiveContent = {
-//   id: 'id',
-//   type: 'markdown',
-//   data: {
-//     text: ''
-//   },
-// };
 
 export default {
   title: `Components / MarkdownCard`,
@@ -25,23 +51,3 @@ export default {
 };
 
 export const Basic = ({ content }: MarkdownCardProps) => <MarkdownCard content={content} />;
-
-// export const CheatSheet = () => {
-//   // const reader = new FileReader();
-//   // reader.onload = (e) => {
-//   //   const text = e.target.result;
-//   //   console.log(text);
-//   // };
-//   // const cheatsheet=reader.readAsText('./examples/cheatsheet.md');
-//   // const text = fs.readFileSync('./examples/cheatsheet.md','utf-8')
-//   const text = 'test';
-//   const content = {
-//     id: 'id',
-//     type: 'markdown',
-//     data: {
-//       "text":{text},
-//     },
-//   };
-
-//   return <MarkdownCard content={content} />;
-// };
