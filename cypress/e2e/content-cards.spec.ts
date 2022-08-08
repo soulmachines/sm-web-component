88c5761b3f151d467589377cd2ae847e2d00224c
Allan Pope <allan.pope@soulmachines.com>
2022-08-08 15:04:38 +1200

    tests: add cypress tests

--- cypress/e2e/content-cards.spec.ts
+++ cypress/e2e/content-cards.spec.ts
@@ -1,12 +1,12 @@
-describe('content cards', () => {
-  const corpusCommands = {
-    optionsCard: '47',
-    imageCard: '46',
-    externalLink: '48',
-    internalLink: '48c',
-    markdownCard: '49',
-  };
+export const corpusCommands = {
+  optionsCard: '47',
+  imageCard: '46',
+  externalLink: '48',
+  internalLink: '48c',
+  markdownCard: '49',
+};
 
+describe('content cards', () => {
   before(() => {
     cy.visit('/');
     cy.launchScene();
---
+++ cypress/e2e/conversation-status.spec.ts
@@ -0,0 +1,32 @@
+import { corpusCommands } from './content-cards.spec';
+
+// TODO: verify tests run once sdk implements it
+describe('conversation status', () => {
+  before(() => {
+    cy.visit('/');
+    cy.launchScene();
+  });
+
+  it('renders an idle icon by default', () => {
+    cy.get('span').contains('Digital Person Waiting').should('exist');
+  });
+
+  describe('when a message is sent to the digital person', () => {
+    beforeEach(() => {
+      cy.sendTextMessage(corpusCommands.optionsCard);
+    });
+
+    it('renders a user speaking icon, followed by dp processing icon, then dp speaking icon before ending on dp waiting', () => {
+      cy.get('span').contains('User Speaking').should('exist');
+      cy.get('span').contains('User Speaking').should('not.exist');
+
+      cy.get('title').contains('Digital Person Processing').should('exist');
+      cy.get('title').contains('Digital Person Processing').should('not.exist');
+
+      cy.get('span').contains('Digital Person Speaking').should('exist');
+      cy.get('span').contains('Digital Person Speaking').should('not.exist');
+
+      cy.get('span').contains('Digital Person Waiting').should('exist');
+    });
+  });
+});
