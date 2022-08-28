7588f5342864db0fea8f57de9af51630227f2a27
Allan Pope <allan.pope@soulmachines.com>
2022-08-29 11:08:19 +1200

    feat: generate component

---
+++ src/components/LoadingIndicator/LoadingIndicator.stories.tsx
@@ -0,0 +1,8 @@
+import { LoadingIndicator } from '.';
+
+export default {
+  title: `Components / LoadingIndicator`,
+  component: LoadingIndicator,
+};
+
+export const Basic = () => <LoadingIndicator />;
---
+++ src/components/LoadingIndicator/LoadingIndicator.test.tsx
@@ -0,0 +1,3 @@
+import { LoadingIndicator } from '.';
+
+describe('<LoadingIndicator />', () => {});
