8cf0243f90498729f0f5317c4e19dabf82c29f73
Allan Pope <allan.pope@soulmachines.com>
2022-08-30 13:44:46 +1200

    feat: add example of animation

--- src/components/LoadingIndicator/LoadingIndicator.stories.tsx
+++ src/components/LoadingIndicator/LoadingIndicator.stories.tsx
@@ -1,4 +1,5 @@
-import { LoadingIndicator, LoadingIndicatorProps } from '.';
+import { useState } from 'preact/hooks';
+import { LoadingIndicator, LoadingIndicatorProps, LoadingIndicatorProgress } from '.';
 
 export default {
   title: `Components / LoadingIndicator`,
@@ -6,7 +7,7 @@ export default {
   argTypes: {
     progressTo: {
       control: 'select',
-      options: ['idle', 'thirtyThree', 'sixtySix', 'oneHundred'],
+      options: ['idle', 'thirtyThree', 'sixtySix', 'oneHundred', 'completed'],
       defaultValue: 'idle',
     },
   },
@@ -15,3 +16,35 @@ export default {
 export const Basic = ({ progressTo }: LoadingIndicatorProps) => (
   <LoadingIndicator progressTo={progressTo} />
 );
+
+export const Example = () => {
+  const [progressState, setProgressState] = useState(LoadingIndicatorProgress.idle);
+
+  const startAnimation = () => {
+    // Reset state
+    setProgressState(LoadingIndicatorProgress.idle);
+
+    setTimeout(() => {
+      setProgressState(LoadingIndicatorProgress.thirtyThree);
+    }, 500);
+
+    setTimeout(() => {
+      setProgressState(LoadingIndicatorProgress.sixtySix);
+    }, 1000);
+
+    setTimeout(() => {
+      setProgressState(LoadingIndicatorProgress.oneHundred);
+    }, 2000);
+
+    setTimeout(() => {
+      setProgressState(LoadingIndicatorProgress.completed);
+    }, 3000);
+  };
+
+  return (
+    <div>
+      <button onClick={startAnimation}>Start</button>
+      <LoadingIndicator progressTo={progressState} />
+    </div>
+  );
+};
