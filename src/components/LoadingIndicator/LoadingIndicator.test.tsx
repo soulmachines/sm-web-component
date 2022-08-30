4715aad97266ca3ccbc8bd837fa18bacef36b7b4
Allan Pope <allan.pope@soulmachines.com>
2022-08-30 12:06:11 +1200

    tests:unit test loader

--- src/components/LoadingIndicator/LoadingIndicator.test.tsx
+++ src/components/LoadingIndicator/LoadingIndicator.test.tsx
@@ -1,3 +1,13 @@
-import { LoadingIndicator } from '.';
+import { render } from '@testing-library/preact';
+import { LoadingIndicator, LoadingIndicatorProgress } from '.';
 
-describe('<LoadingIndicator />', () => {});
+describe('<LoadingIndicator />', () => {
+  it('renders the loader with a role of progressbar', () => {
+    const { getByRole } = render(
+      <LoadingIndicator progressTo={LoadingIndicatorProgress.thirtyThree} />,
+    );
+    expect(getByRole('progressbar')).toBeInTheDocument();
+  });
+
+  describe('when it is completed', () => {});
+});
