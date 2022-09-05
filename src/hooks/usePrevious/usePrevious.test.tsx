3f78c4a582446832f8c84fd134a1bf8d6c7c0106
Allan Pope <allan.pope@soulmachines.com>
2022-09-06 09:37:45 +1200

    feat: add hook to store previous value

---
+++ src/hooks/usePrevious/usePrevious.test.tsx
@@ -0,0 +1,23 @@
+import { renderHook } from '@testing-library/preact';
+import { usePrevious } from './usePrevious';
+
+describe('usePrevious()', () => {
+  it('returns the initial value', () => {
+    const { result } = renderHook(() => usePrevious(10));
+    expect(result.current).toEqual(10);
+  });
+
+  it('returns the previous value when the value changes', () => {
+    const { result, rerender } = renderHook((value: string = 'one') => usePrevious(value));
+    expect(result.current).toEqual('one');
+
+    rerender('two');
+    expect(result.current).toEqual('one');
+
+    rerender('three');
+    expect(result.current).toEqual('two');
+
+    rerender('four');
+    expect(result.current).toEqual('three');
+  });
+});
---
+++ src/hooks/usePrevious/usePrevious.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useRef } from 'preact/hooks';
+
+// from: https://usehooks.com/usePrevious/
+function usePrevious(value: string | number) {
+  const ref = useRef(value);
+
+  // Only re-run if value changes
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  // Return previous value (happens before update in useEffect above)
+  return ref.current;
+}
+
+export { usePrevious };
