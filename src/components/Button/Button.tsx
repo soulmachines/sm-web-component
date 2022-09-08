ae19ae784acbbd158d1663cc714e1966888121cd
Allan Pope <allan.pope@soulmachines.com>
2022-09-08 16:05:03 +1200

    refactor: use new primary color names

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -13,9 +13,9 @@ export type ButtonProps = {
 
 export function Button({ children, theme, onClick }: ButtonProps) {
   const buttonClass = classNames({
-    'sm-cursor-pointer sm-px-4 sm-py-2 sm-font-primary sm-text-sm sm-tracking-wider sm-transition-colors sm-uppercase sm-font-medium sm-outline disabled:sm-bg-primary-200  disabled:sm-cursor-not-allowed md:sm-text-base':
+    'sm-cursor-pointer sm-px-4 sm-py-2 sm-font-primary sm-text-sm sm-tracking-wider sm-transition-colors sm-uppercase sm-font-medium sm-outline disabled:sm-bg-primary-light disabled:sm-cursor-not-allowed md:sm-text-base':
       true,
-    'sm-text-white sm-rounded-lg sm-bg-primary-600 hover:sm-bg-primary-700 active:sm-bg-primary-800 focus:sm-bg-primary-600 focus:sm-outline-2 focus:sm-outline-primary-200':
+    'sm-text-white sm-rounded-lg sm-bg-primary-base hover:sm-bg-primary-dark active:sm-bg-primary-dark focus:sm-bg-primary-dark focus:sm-outline-2 focus:sm-outline-primary-light':
       theme === ButtonTheme.default,
     'sm-text-left sm-flex sm-justify-between sm-items-center sm-bg-transparent sm-rounded-sm sm-text-black sm-capitalize sm-font-normal sm-border-gray-light sm-border sm-border-solid sm-transition-colors sm-outline-transparent sm-outline-1 sm-outline-offset-[-2px] hover:sm-border-primary-base hover:sm-outline-primary-base focus:sm-border-primary-base focus:sm-outline-primary-base active:sm-text-white active:sm-bg-primary-base':
       theme === ButtonTheme.outline,
