20511303e5d436fd33d7f9e320d36ceee203cced
Allan Pope <allan.pope@soulmachines.com>
2022-09-09 12:54:33 +1200

    fix: remove button border

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -13,7 +13,7 @@ export type ButtonProps = {
 
 export function Button({ children, theme, onClick }: ButtonProps) {
   const buttonClass = classNames({
-    'sm-cursor-pointer sm-px-4 sm-py-2 sm-font-primary sm-text-sm sm-tracking-wider sm-transition-colors sm-uppercase sm-font-medium sm-outline disabled:sm-bg-primary-light disabled:sm-cursor-not-allowed md:sm-text-base':
+    'sm-cursor-pointer sm-px-4 sm-py-2 sm-font-primary sm-text-sm sm-tracking-wider sm-transition-colors sm-border-none sm-uppercase sm-font-medium sm-outline disabled:sm-bg-primary-light disabled:sm-cursor-not-allowed md:sm-text-base':
       true,
     'sm-text-white sm-rounded-lg sm-bg-primary-base hover:sm-bg-primary-dark active:sm-bg-primary-dark focus:sm-bg-primary-dark focus:sm-outline-2 focus:sm-outline-primary-light':
       theme === ButtonTheme.default,
