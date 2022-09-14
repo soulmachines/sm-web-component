acee4b8cd4ccb78542fa65a8e7b6d32c0fc3ad3d
Allan Pope <allan.pope@soulmachines.com>
2022-09-15 11:34:49 +1200

    refactor: use shared types

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -1,12 +1,10 @@
-import { Text } from '.';
-import { TextProps } from './Text';
+import { Text, TextProps, textSizes } from '.';
 
-export const textOptions: TextProps['size'][] = ['sm', 'md', 'lg'];
 export default {
   title: `Components / Text`,
   component: Text,
   argTypes: {
-    size: { control: 'select', options: textOptions },
+    size: { control: 'select', options: textSizes },
     children: {
       type: { name: 'string' },
       defaultValue: 'Your session has ended. You can reconnect anytime you are ready.',
