89321e9ae596bf1c14398504b547e58859a55081
Yvonne Yang <69659832+yvonne-yy@users.noreply.github.com>
2022-10-10 12:20:56 +1300

    use classNames to toggle styling between fixed and embedded widget

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -31,13 +31,76 @@ export function Widget({
   const isConnectingOrConnected = connectionStatus === ConnectionStatus.CONNECTING || isConnected;
   const isDisconnected = connectionStatus === ConnectionStatus.DISCONNECTED;
 
-  const wrapperPositionClass = classNames({
-    'sm-right-0': position === widgetPosition.BOTTOM_RIGHT,
-    'sm-left-0': position === widgetPosition.BOTTOM_LEFT,
-  });
-  const notificationVideoOrderClass = classNames({
-    'sm-flex-row-reverse': position === widgetPosition.BOTTOM_LEFT,
-  });
+  const styles = {
+    scaleAnimation: useSpring({
+      transform: isConnectingOrConnected ? 'scale(2)' : 'scale(1)',
+      config: config.stiff,
+    }),
+
+    // Scales down the above animation back to 1, the normal size
+    scaledDownClass: classNames({
+      'sm-scale-50 sm-origin-bottom-right': isConnectingOrConnected,
+    }),
+
+    widgetPositionClass: classNames({
+      'sm-right-0': position === widgetPosition.BOTTOM_RIGHT && layout === widgetLayout.FIXED,
+      'sm-left-0': position === widgetPosition.BOTTOM_LEFT && layout === widgetLayout.FIXED,
+    }),
+
+    containerClass: classNames({
+      'sm-fixed sm-bottom-0 sm-p-2 sm-text-primary-text sm-z-max sm-pointer-events-none sm-h-full md:sm-p-5':
+        layout === widgetLayout.FIXED,
+      'sm-text-primary-text sm-h-full sm-w-full': layout === widgetLayout.Embedded,
+    }),
+
+    containerPositionClass: classNames({
+      'sm-flex sm-flex-col sm-gap-y-2 sm-h-full sm-justify-end md:sm-gap-y-5':
+        layout === widgetLayout.FIXED,
+      'sm-rounded-xl sm-origin-bottom-right sm-bg-white sm-pointer-events-auto md:sm-rounded-3xl':
+        layout === widgetLayout.Embedded,
+    }),
+
+    notificationClass: classNames({
+      'sm-flex sm-flex-wrap sm-gap-2 sm-items-center sm-justify-end md:sm-gap-5':
+        layout === widgetLayout.FIXED,
+      'sm-flex-row-reverse':
+        layout === widgetLayout.FIXED && position === widgetPosition.BOTTOM_LEFT,
+    }),
+
+    greetingClass: classNames({
+      'sm-max-w-xs sm-z-10': layout === widgetLayout.FIXED,
+    }),
+
+    scaleAnimationClass: classNames({
+      'sm-rounded-xl sm-origin-bottom-right sm-shadow-lg sm-bg-white sm-pointer-events-auto md:sm-rounded-3xl':
+        layout === widgetLayout.FIXED,
+    }),
+
+    connectButtonClass: classNames({
+      'sm-flex sm-justify-center sm-items-center sm-rounded-inherit sm-text-primary-base sm-border-none sm-outline sm-outline-2 sm-outline-transparent sm-bg-transparent hover:sm-outline-secondary-base sm-transition-colors sm-overflow-hidden':
+        true,
+      'sm-w-18 sm-h-18 md:sm-w-35 md:sm-h-35 ': layout === widgetLayout.FIXED,
+      'sm-w-full sm-h-full': layout === widgetLayout.Embedded,
+    }),
+
+    connectingOrConnectedClass: classNames({
+      'sm-relative sm-rounded-inherit sm-overflow-hidden sm-transform-gpu':
+        layout === widgetLayout.FIXED,
+      'sm-w-63 sm-h-40 md:sm-h-54 md:sm-w-88 sm-border-2 sm-border-solid sm-border-gray-lightest':
+        isConnectingOrConnected && layout === widgetLayout.FIXED,
+
+      'sm-w-full sm-h-full sm-relative sm-rounded-inherit sm-overflow-hidden sm-transform-gpu':
+        layout === widgetLayout.Embedded,
+      'sm-absolute': !isConnectingOrConnected && layout === widgetLayout.Embedded,
+      'sm-border-2 sm-border-solid sm-border-gray-lightest':
+        isConnectingOrConnected && layout === widgetLayout.Embedded,
+    }),
+
+    contentCardsClass: classNames({
+      'sm-w-63 md:sm-w-88 sm-max-h-full sm-flex sm-flex-col sm-justify-end sm-gap-y-2 sm-overflow-hidden sm-p-8 -sm-m-8 sm-box-content md:sm-gap-y-3':
+        layout === widgetLayout.FIXED,
+    }),
+  };
 
   // Connect directly if it's resume session
   useEffect(() => {
@@ -61,97 +124,53 @@ export function Widget({
     </div>
   );
 
-  const scaleAnimation = useSpring({
-    transform: isConnectingOrConnected ? 'scale(2)' : 'scale(1)',
-    config: config.stiff,
-  });
-
-  // Scales down the above animation back to 1, the normal size
-  const scaledDownClass = classNames({
-    'sm-scale-50 sm-origin-bottom-right': isConnectingOrConnected,
-  });
-
-  if (layout === widgetLayout.Embedded) {
-    return (
-      <div>
-        <div className="sm-text-primary-text sm-h-full sm-w-full ">
-          <div className="sm-rounded-xl sm-origin-bottom-right sm-bg-white sm-pointer-events-auto md:sm-rounded-3xl">
-            {isDisconnected && (
-              <button
-                onClick={connect}
-                data-sm-cy="connectButton"
-                className="sm-w-full sm-h-full sm-flex sm-justify-center sm-items-center sm-rounded-inherit sm-text-primary-base sm-border-none sm-outline sm-outline-2 sm-outline-transparent sm-bg-transparent hover:sm-outline-secondary-base sm-transition-colors sm-overflow-hidden"
-              >
-                <ProfileImage src={profilePicture} />
-              </button>
-            )}
-
-            <div
-              className={classNames({
-                'sm-w-full sm-h-full sm-relative sm-rounded-inherit sm-overflow-hidden sm-transform-gpu':
-                  true,
-                ' sm-absolute': !isConnectingOrConnected,
-                'sm-border-2 sm-border-solid sm-border-gray-lightest': isConnectingOrConnected,
-              })}
-            >
-              <Video autoConnect={false} loadingIndicator={<LoadingIndicator />} />
-              {isConnected && <VideoControls />}
-            </div>
-          </div>
-        </div>
-
-        <div class="sm-w-63 md:sm-w-88 sm-max-h-full sm-flex sm-flex-col sm-justify-end sm-gap-y-2 sm-overflow-hidden sm-p-8 -sm-m-8 sm-box-content md:sm-gap-y-3">
-          <ContentCards />
-        </div>
+  const greetingElement =
+    !isConnectingOrConnected && layout === widgetLayout.FIXED ? (
+      <div className={styles.greetingClass}>
+        <Notifications greeting={greeting} />
       </div>
+    ) : (
+      <></>
     );
-  }
+
+  const videoElement = (
+    <>
+      {isDisconnected && (
+        <button onClick={connect} data-sm-cy="connectButton" className={styles.connectButtonClass}>
+          <ProfileImage src={profilePicture} />
+        </button>
+      )}
+      <div className={styles.connectingOrConnectedClass}>
+        <Video autoConnect={false} loadingIndicator={<LoadingIndicator />} />
+        {isConnected && <VideoControls />}
+      </div>
+    </>
+  );
+
+  const animatedVideoElement =
+    layout === widgetLayout.FIXED ? (
+      <div className={styles.scaledDownClass}>
+        <animated.div style={styles.scaleAnimation} className={styles.scaleAnimationClass}>
+          {videoElement}
+        </animated.div>
+      </div>
+    ) : (
+      videoElement
+    );
+
+  const contentCardsElement = (
+    <div class={styles.contentCardsClass}>
+      <ContentCards />
+    </div>
+  );
 
   return (
-    <div
-      className={`sm-fixed sm-bottom-0 sm-p-2 sm-text-primary-text sm-z-max sm-pointer-events-none sm-h-full md:sm-p-5 ${wrapperPositionClass}`}
-    >
-      <div className="sm-flex sm-flex-col sm-gap-y-2 sm-h-full sm-justify-end md:sm-gap-y-5">
-        <div class="sm-w-63 md:sm-w-88 sm-max-h-full sm-flex sm-flex-col sm-justify-end sm-gap-y-2 sm-overflow-hidden sm-p-8 -sm-m-8 sm-box-content md:sm-gap-y-3">
-          <ContentCards />
-        </div>
-
-        <div
-          className={`sm-flex sm-flex-wrap sm-gap-2 sm-items-center sm-justify-end md:sm-gap-5 ${notificationVideoOrderClass}`}
-        >
-          {!isConnectingOrConnected && (
-            <div className="sm-max-w-xs sm-z-10">
-              <Notifications greeting={greeting} />
-            </div>
-          )}
-
-          <div className={scaledDownClass}>
-            <animated.div
-              style={scaleAnimation}
-              className="sm-rounded-xl sm-origin-bottom-right sm-shadow-lg sm-bg-white sm-pointer-events-auto md:sm-rounded-3xl"
-            >
-              {isDisconnected && (
-                <button
-                  onClick={connect}
-                  data-sm-cy="connectButton"
-                  className="sm-w-18 sm-h-18 md:sm-w-35 md:sm-h-35 sm-flex sm-justify-center sm-items-center sm-rounded-inherit sm-text-primary-base sm-border-none sm-outline sm-outline-2 sm-outline-transparent sm-bg-transparent hover:sm-outline-secondary-base sm-transition-colors sm-overflow-hidden"
-                >
-                  <ProfileImage src={profilePicture} />
-                </button>
-              )}
-
-              <div
-                className={classNames({
-                  'sm-relative sm-rounded-inherit sm-overflow-hidden sm-transform-gpu': true,
-                  'sm-w-63 sm-h-40 md:sm-h-54 md:sm-w-88 sm-border-2 sm-border-solid sm-border-gray-lightest':
-                    isConnectingOrConnected,
-                })}
-              >
-                <Video autoConnect={false} loadingIndicator={<LoadingIndicator />} />
-                {isConnected && <VideoControls />}
-              </div>
-            </animated.div>
-          </div>
+    <div className={`${styles.containerClass} ${styles.widgetPositionClass}`}>
+      <div className={styles.containerPositionClass}>
+        {contentCardsElement}
+        <div className={`${styles.notificationClass}`}>
+          {greetingElement}
+          {animatedVideoElement}
         </div>
       </div>
     </div>
