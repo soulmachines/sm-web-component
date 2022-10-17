9008a88fd35a0b81b5aa369292768c95bd3182c8
Allan Pope <allan.pope@soulmachines.com>
2022-10-18 08:22:57 +1300

    refactor: remove unneeded css rule (#333)

--- src/appComponents/Widget/Widget.tsx
+++ src/appComponents/Widget/Widget.tsx
@@ -102,7 +102,7 @@ export function Widget({
                 <button
                   onClick={connect}
                   data-sm-cy="connectButton"
-                  className="sm-w-18 sm-h-18 md:sm-w-35 md:sm-h-35 sm-flex sm-justify-center sm-items-center sm-rounded-inherit sm-text-primary-base sm-border-none sm-ring sm-ring-2 sm-ring-transparent sm-bg-transparent hover:sm-ring-secondary-base sm-transition-colors sm-overflow-hidden"
+                  className="sm-w-18 sm-h-18 md:sm-w-35 md:sm-h-35 sm-flex sm-justify-center sm-items-center sm-rounded-inherit sm-text-primary-base sm-border-none sm-ring sm-ring-transparent sm-bg-transparent hover:sm-ring-secondary-base sm-transition-colors sm-overflow-hidden"
                 >
                   <ProfileImage src={profilePicture} />
                 </button>
