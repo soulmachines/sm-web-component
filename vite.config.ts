2129ed70adae6a68d82d3340ac10631f4954cb1d
Allan Pope <allan.pope@soulmachines.com>
2022-10-27 15:27:04 +1300

    fix: use different config for serving the app tp building it quic-2246 (#338)

--- vite.config.ts
+++ vite.config.ts
@@ -9,21 +9,13 @@ import dns from 'dns';
 dns.setDefaultResultOrder('verbatim');
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode, command }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '');
   const version = env.VERSION || new Date().getTime();
-  let developmentPlugins = [];
 
-  if (mode === 'development') {
-    developmentPlugins = [
-      // Injects env into our example files
-      createHtmlPlugin({ template: path.resolve(__dirname, 'index.html') }),
-    ];
-  }
-
-  return {
+  const defaultConfig = {
     define: {
       // As of vite 3, process.env are kept in library mode
       // Replaces process.env.NODE_ENV in the code with the current mode (eg: production or development)
@@ -33,21 +25,11 @@ export default defineConfig(({ mode }) => {
       // Ignore warning https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
       logOverride: { 'this-is-undefined-in-esm': 'silent' },
     },
-    plugins: [preact(), ...developmentPlugins],
-    server: {
-      open: '/index.html',
-      port: 3000,
-    },
+    plugins: [preact()],
     preview: {
       port: 5050,
     },
     build: {
-      manifest: true,
-      lib: {
-        entry: path.resolve(__dirname, 'src/web-components'),
-        name: 'soulmachines-web-components',
-        formats: ['umd'],
-      },
       rollupOptions: {
         output: {
           entryFileNames: `web-components.${version}.js`,
@@ -56,4 +38,43 @@ export default defineConfig(({ mode }) => {
       },
     },
   };
+
+  // Config split as issue with vite deleting the index.html file
+  if (command === 'build') {
+    // Production builds UMD bundle and generates a manifest
+    return {
+      ...defaultConfig,
+      build: {
+        ...defaultConfig.build,
+        manifest: true,
+        lib: {
+          entry: path.resolve(__dirname, 'src/web-components'),
+          name: 'soulmachines-web-components',
+          formats: ['umd'],
+        },
+      },
+    };
+  } else {
+    // When serving the code locally use es modules and run the HTML plugin
+    return {
+      ...defaultConfig,
+      plugins: [
+        ...defaultConfig.plugins,
+        // Injects env into our example files
+        createHtmlPlugin({ template: path.resolve(__dirname, 'index.html') }),
+      ],
+      server: {
+        open: '/index.html',
+        port: 3000,
+      },
+      build: {
+        rollupOptions: {
+          ...defaultConfig.build.rollupOptions,
+          input: {
+            'web-components': path.resolve(__dirname, 'src', 'web-components'),
+          },
+        },
+      },
+    };
+  }
 });
