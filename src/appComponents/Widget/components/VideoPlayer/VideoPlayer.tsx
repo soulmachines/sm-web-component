81246e5945970ac3f9824d0fae74060a31b9b5aa
Allan Pope <allan.pope@soulmachines.com>
2022-11-03 17:14:33 +1300

    fix: full frame in safari

--- src/appComponents/Widget/components/VideoPlayer/VideoPlayer.tsx
+++ src/appComponents/Widget/components/VideoPlayer/VideoPlayer.tsx
@@ -13,17 +13,22 @@ export const VideoPlayer = ({ renderInFullFrame, floatingPosition }: VideoPlayer
   const { connectionStatus } = useSoulMachines();
   const isConnected = connectionStatus === ConnectionStatus.CONNECTED;
 
+  // Keeps the container in the page for when we go from fullframe to float.
+  // Otherwise we loose the position when the elements fixed and we get a weird layout shift
+  // Need to remove it when in full frame mode as it wasnt appearing full width in safari
+  const wrapperClasses = classNames({
+    'sm-floating-container': !renderInFullFrame,
+  });
+
   const videoContainerClasses = classNames({
-    'sm-transition-all sm-duration-300 sm-floating-container': true,
-    'sm-fixed sm-bottom-0 sm-w-full sm-h-full sm-p-5': renderInFullFrame,
+    'sm-transition-all sm-duration-300 sm-w-full sm-h-full': true,
+    'sm-fixed sm-bottom-0 sm-p-5': renderInFullFrame,
     'sm-left-0': floatingPosition === widgetPosition.BOTTOM_LEFT,
     'sm-right-0': floatingPosition === widgetPosition.BOTTOM_RIGHT,
   });
 
   return (
-    // Keeps the container in the page for when we go from fullframe to float.
-    // Prevents a shift in the layout when the video goes from fixed position to static
-    <div className="sm-floating-container" hidden={!isConnected} aria-hidden={!isConnected}>
+    <div className={wrapperClasses} hidden={!isConnected} aria-hidden={!isConnected}>
       <div className={videoContainerClasses}>
         <div className="sm-w-full sm-h-full sm-round-shadow-box sm-border-2 sm-border-solid sm-border-gray-lightest">
           <Video autoConnect={false} />
