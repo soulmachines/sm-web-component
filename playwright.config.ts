e84910fb4f0bb2cd14acf3df491f99b6d02a13a5
Allan Pope <allan.pope@soulmachines.com>
2022-11-07 16:13:39 +1300

    feat: save images on failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,6 +38,8 @@ const config: PlaywrightTestConfig = {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
+    screenshot: 'only-on-failure',
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -81,7 +83,7 @@ const config: PlaywrightTestConfig = {
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+  outputDir: 'e2e-results/',
 
   /* Run your local dev server before starting the tests */
   webServer: {
