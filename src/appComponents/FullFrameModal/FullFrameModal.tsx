054f5ec82b0cfc78ebaccd1961387a69ee2b832c
John Niedfeldt <john.niedfeldt@soulmachines.com>
2022-11-16 14:01:37 -0700

    Name change

--- src/appComponents/FullFrameModal/FullFrameModal.tsx
+++ src/appComponents/FullFrameModal/FullFrameModal.tsx
@@ -1,5 +1,5 @@
 import { Dialog } from '@headlessui/react';
-import { DpBlur } from '../../components/DpBlur';
+import { BackdropBlur } from '../../components/BackdropBlur';
 import { useSoulMachines } from '../../contexts/SoulMachinesContext';
 import { Video } from '../Video';
 import { VideoControls } from '../VideoControls';
@@ -11,9 +11,10 @@ export type FullFrameModalProps = {
 
 export function FullFrameModal({ isOpen }: FullFrameModalProps) {
   const { toggleLayout } = useSoulMachines();
+  const ref = useRef(null);
 
   return (
-    <Dialog open={isOpen} onClose={() => toggleLayout()} className="sm-widget">
+    <Dialog ref={ref} open={isOpen} onClose={() => toggleLayout()} className="sm-widget">
       <Dialog.Title className="sm-sr-only">Interactive Digital Person</Dialog.Title>
 
       {/* The backdrop, rendered as a fixed sibling to the panel container */}
@@ -23,7 +24,7 @@ export function FullFrameModal({ isOpen }: FullFrameModalProps) {
         <Dialog.Panel className="sm-w-full sm-h-full md:sm-rounded-3xl sm-overflow-hidden sm-bg-white sm-transform-gpu sm-border-2 sm-border-solid sm-border-gray-lightest">
           <Video autoConnect={false} />
           <VideoControls />
-          {isOpen && <DpBlur scrollTargetRef={ref} />}
+          {isOpen && <BackdropBlur scrollTargetRef={ref} />}
         </Dialog.Panel>
       </div>
     </Dialog>
--- src/components/DpBlur/DpBlur.test.tsx
+++
@@ -1,3 +0,0 @@
-import { DpBlur } from '.';
-
-describe('<DpBlur />', () => {});
