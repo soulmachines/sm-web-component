9fc389884a291a9b48a4c7d9e802eb7b585256c1
Allan Pope <allan.pope@soulmachines.com>
2022-11-17 09:20:31 +1300

    feat: position content cards and wire up blur  quic-2276

--- src/contentCards/LinkCard/LinkCard.tsx
+++ src/contentCards/LinkCard/LinkCard.tsx
@@ -31,15 +31,11 @@ export function LinkCard({ content, isExternal, style }: LinkCardProps) {
     <Card style={style}>
       <div
         data-sm-content={content.id}
-        className="sm-flex sm-flex-col sm-gap-y-3 sm-items-start sm-h-full sm-max-h-contentCard sm-overflow-y-auto"
+        className="sm-flex sm-flex-col sm-gap-y-3 sm-items-start sm-h-full sm-overflow-y-auto sm-content-card-max-height"
       >
         {data.imageUrl && <img src={data.imageUrl} alt={data.title} />}
         <Heading type="h2">{data.title}</Heading>
-        {data.description && (
-          <div className="sm-hidden md:sm-block">
-            <Text>{data.description}</Text>
-          </div>
-        )}
+        {data.description && <Text>{data.description}</Text>}
         <div className="sm-bg-white sm-sticky sm-bottom-0 sm-w-full sm-pt-5 sm-border-solid sm-border-0 sm-border-t-2 sm-border-gray-lightest">
           <a className="sm-text-white sm-no-underline" href={data.url} {...conditionalAttributes}>
             <Button>View Page</Button>
