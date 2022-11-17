d7c220a958b6f64e4fd3c1844650b1c409fbb995
John Niedfeldt <john.niedfeldt@soulmachines.com>
2022-11-16 19:24:55 -0700

    PR comment resolution

--- src/components/BackdropBlur/BackdropBlur.test.tsx
+++ src/components/BackdropBlur/BackdropBlur.test.tsx
@@ -5,11 +5,11 @@ import { imageCardContent } from '../../storybook-content';
 import { ImageCard } from '../../contentCards/ImageCard';
 
 const ref = { current: null };
-const scrollElTestId = 'scrollEl';
+const scrollableContainerId = 'scrollEl';
 
 function renderBackdropBlur(scrollOffset = 100) {
   const { getByTestId, container } = render(
-    <div ref={ref} data-testid={scrollElTestId}>
+    <div ref={ref} data-testid={scrollableContainerId}>
       <BackdropBlur scrollTargetRef={ref} scrollOffset={scrollOffset}>
         <ImageCard content={imageCardContent} />
       </BackdropBlur>
@@ -22,25 +22,29 @@ const minimumScrollToRenderBlur = 100;
 describe('<BackdropBlur />', () => {
   it('adds the blur class when scroll amount is greater than the scrollOffset', () => {
     const { getByTestId, container } = renderBackdropBlur(minimumScrollToRenderBlur);
-    const backgroundblur = getByTestId(scrollElTestId);
-    jest.spyOn(backgroundblur, 'scrollTop', 'get').mockReturnValue(minimumScrollToRenderBlur + 1);
-    fireEvent.scroll(backgroundblur);
+    const scrollableContainer = getByTestId(scrollableContainerId);
+    jest
+      .spyOn(scrollableContainer, 'scrollTop', 'get')
+      .mockReturnValue(minimumScrollToRenderBlur + 1);
+    fireEvent.scroll(scrollableContainer);
     expect(container.querySelectorAll('.sm-backdrop-blur-lg')).toHaveLength(1);
   });
 
   it('does not add the blur class when scroll amount is less than the scrollOffset', () => {
     const { getByTestId, container } = renderBackdropBlur(minimumScrollToRenderBlur);
-    const backgroundblur = getByTestId(scrollElTestId);
-    jest.spyOn(backgroundblur, 'scrollTop', 'get').mockReturnValue(minimumScrollToRenderBlur - 1);
-    fireEvent.scroll(backgroundblur);
+    const scrollableContainer = getByTestId(scrollableContainerId);
+    jest
+      .spyOn(scrollableContainer, 'scrollTop', 'get')
+      .mockReturnValue(minimumScrollToRenderBlur - 1);
+    fireEvent.scroll(scrollableContainer);
     expect(container.querySelectorAll('.sm-backdrop-blur-lg')).toHaveLength(0);
   });
 
   it('adds the blur class when scroll amount is equal to the scrollOffset', () => {
     const { getByTestId, container } = renderBackdropBlur(minimumScrollToRenderBlur);
-    const backgroundblur = getByTestId(scrollElTestId);
-    jest.spyOn(backgroundblur, 'scrollTop', 'get').mockReturnValue(minimumScrollToRenderBlur);
-    fireEvent.scroll(backgroundblur);
+    const scrollableContainer = getByTestId(scrollableContainerId);
+    jest.spyOn(scrollableContainer, 'scrollTop', 'get').mockReturnValue(minimumScrollToRenderBlur);
+    fireEvent.scroll(scrollableContainer);
     expect(container.querySelectorAll('.sm-backdrop-blur-lg')).toHaveLength(1);
   });
 });
