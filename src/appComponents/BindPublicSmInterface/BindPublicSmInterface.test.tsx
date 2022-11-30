a56340d0121d4ddebeb40958aa3de0182c72cb3f
Allan Pope <allan.pope@soulmachines.com>
2022-11-30 16:04:11 +1300

    feat: always bind public methods quic-2370 (#370)
    * feat: always bind public methods quic-2370
    
    * fix: remove unused var

--- src/appComponents/BindPublicSmInterface/BindPublicSmInterface.test.tsx
+++ src/appComponents/BindPublicSmInterface/BindPublicSmInterface.test.tsx
@@ -1,7 +1,6 @@
 import { render } from '@testing-library/preact';
 import { BindPublicSmInterface } from '.';
 import * as SoulMachinesContext from '../../contexts/SoulMachinesContext';
-import { ConnectionStatus } from '../../enums';
 import { SMWidgetElement } from '../../web-components/sm-widget/SMWidget/SMWidget';
 
 jest.mock('../../contexts/SoulMachinesContext/SoulMachinesContext');
@@ -9,93 +8,17 @@ jest.mock('../../contexts/SoulMachinesContext/SoulMachinesContext');
 describe('<BindPublicSmInterface />', () => {
   const element: SMWidgetElement = document.createElement('div');
 
-  describe(`when it is initialized`, () => {
-    beforeEach(() => {
-      jest.spyOn(SoulMachinesContext, 'useSoulMachines').mockReturnValue({
-        ...SoulMachinesContext.useSoulMachines(),
-      });
-    });
+  it('exposes `persona` as a property of the element', () => {
+    const { persona } = SoulMachinesContext.useSoulMachines();
+    render(<BindPublicSmInterface element={element} />);
 
-    it('exposes `persona` as a property of the element', () => {
-      const { persona } = SoulMachinesContext.useSoulMachines();
-      render(<BindPublicSmInterface element={element} />);
-
-      expect(element.persona).toBe(persona);
-    });
-
-    it('exposes `scene` as a property of the element', () => {
-      const { scene } = SoulMachinesContext.useSoulMachines();
-      render(<BindPublicSmInterface element={element} />);
-
-      expect(element.scene).toBe(scene);
-    });
+    expect(element.persona).toBe(persona);
   });
 
-  const whenNotConnectingOrConnected = [
-    { description: 'disconnected', statusEnum: ConnectionStatus.DISCONNECTED },
-    { description: 'timed out', statusEnum: ConnectionStatus.TIMED_OUT },
-    { description: 'errored', statusEnum: ConnectionStatus.ERRORED },
-  ];
+  it('exposes `scene` as a property of the element', () => {
+    const { scene } = SoulMachinesContext.useSoulMachines();
+    render(<BindPublicSmInterface element={element} />);
 
-  whenNotConnectingOrConnected.forEach(({ description, statusEnum }) => {
-    describe(`when it is ${description}`, () => {
-      beforeEach(() => {
-        jest.spyOn(SoulMachinesContext, 'useSoulMachines').mockReturnValue({
-          ...SoulMachinesContext.useSoulMachines(),
-          connectionStatus: statusEnum,
-        });
-      });
-
-      it('does not call sendTextMessage', () => {
-        const { sendTextMessage } = SoulMachinesContext.useSoulMachines();
-        render(<BindPublicSmInterface element={element} />);
-
-        element.sendTextMessage?.('test');
-        expect(sendTextMessage).not.toHaveBeenCalled();
-      });
-
-      it('does not call enableDebugLogging', () => {
-        const { enableDebugLogging } = SoulMachinesContext.useSoulMachines();
-        render(<BindPublicSmInterface element={element} />);
-
-        element.enableDebugLogging?.(true);
-        expect(enableDebugLogging).not.toHaveBeenCalled();
-      });
-    });
-  });
-
-  const whenConnectingOrConnected = [
-    { description: 'connecting', statusEnum: ConnectionStatus.CONNECTING },
-    { description: 'connected', statusEnum: ConnectionStatus.CONNECTED },
-  ];
-
-  whenConnectingOrConnected.forEach(({ description, statusEnum }) => {
-    describe(`when it is ${description}`, () => {
-      beforeEach(() => {
-        jest.spyOn(SoulMachinesContext, 'useSoulMachines').mockReturnValue({
-          ...SoulMachinesContext.useSoulMachines(),
-          connectionStatus: statusEnum,
-        });
-      });
-
-      it('calls sendTextMessage with the text when element.sendTextMessage is called', () => {
-        const { sendTextMessage } = SoulMachinesContext.useSoulMachines();
-        render(<BindPublicSmInterface element={element} />);
-
-        element.sendTextMessage?.('test');
-        expect(sendTextMessage).toHaveBeenCalledWith('test');
-      });
-
-      it('calls enableDebugLogging when element.enableDebugLogging is called', () => {
-        const { enableDebugLogging } = SoulMachinesContext.useSoulMachines();
-        render(<BindPublicSmInterface element={element} />);
-
-        element.enableDebugLogging?.(true);
-        expect(enableDebugLogging).toHaveBeenCalledWith(true);
-
-        element.enableDebugLogging?.(false);
-        expect(enableDebugLogging).toHaveBeenCalledWith(false);
-      });
-    });
+    expect(element.scene).toBe(scene);
   });
 });
--- src/appComponents/BindPublicSmInterface/BindPublicSmInterface.tsx
+++ src/appComponents/BindPublicSmInterface/BindPublicSmInterface.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'preact/hooks';
 import { useSoulMachines } from '../../contexts/SoulMachinesContext';
-import { ConnectionStatus } from '../../enums';
 import { SMWidgetElement } from '../../web-components/sm-widget/SMWidget/SMWidget';
 
 export type BindPublicSmInterfaceProps = {
@@ -12,8 +11,7 @@ export type BindPublicSmInterfaceProps = {
 type GenericFunction = Function;
 
 export function BindPublicSmInterface({ element }: BindPublicSmInterfaceProps) {
-  const { connectionStatus, persona, scene, sendTextMessage, enableDebugLogging } =
-    useSoulMachines();
+  const { persona, scene, sendTextMessage, enableDebugLogging } = useSoulMachines();
 
   useEffect(() => {
     /**
@@ -40,26 +38,10 @@ export function BindPublicSmInterface({ element }: BindPublicSmInterfaceProps) {
       ['enableDebugLogging', enableDebugLogging],
     ];
 
-    const addPublicMethods = () => {
-      publicMethods.map(([name, implementation]) => {
-        htmlElement[name] = implementation;
-      });
-    };
-    const removePublicMethods = () => {
-      publicMethods.map(([name]) => {
-        htmlElement[name] = undefined;
-      });
-    };
-
-    if (
-      connectionStatus === ConnectionStatus.CONNECTING ||
-      connectionStatus === ConnectionStatus.CONNECTED
-    ) {
-      addPublicMethods();
-    } else {
-      removePublicMethods();
-    }
-  }, [element, connectionStatus, sendTextMessage, enableDebugLogging]);
+    publicMethods.map(([name, implementation]) => {
+      htmlElement[name] = implementation;
+    });
+  }, [element, sendTextMessage, enableDebugLogging]);
 
   return null;
 }
