931d2d42d7faa0ced454a5ff6d3e8fe4362703d8
khurram.jawed <khurram.jawed@soulmachines.com>
2024-10-17 14:06:13 +1300

    moved video element to root in widget div

--- src/appComponents/Widget/Widget.tsx
+++ src/appComponents/Widget/Widget.tsx
@@ -54,56 +54,7 @@ export function Widget({
   }, [cards, layout]);
 
   return (
-    <div className="sm-fixed sm-text-primary-text sm-z-max sm-pointer-events-none sm-h-full sm-w-full sm-p-5">
-      <div
-        className={classNames('sm-h-full sm-flex sm-flex-col', {
-          'sm-gap-y-2 md:sm-gap-y-5 sm-justify-end':
-            layout === widgetLayout.FLOAT && position !== widgetPosition.TOP_LEFT,
-          'sm-justify-start': position === widgetPosition.TOP_LEFT,
-        })}
-      >
-        <div
-          className={classNames('sm-flex sm-flex-wrap sm-gap-2 sm-items-center', {
-            'sm-justify-end sm-items-end': position === widgetPosition.BOTTOM_RIGHT,
-            'sm-justify-start sm-items-start':
-              position === widgetPosition.BOTTOM_LEFT || position === widgetPosition.TOP_LEFT,
-            'sm-flex-row-reverse': position === widgetPosition.BOTTOM_LEFT,
-          })}
-        >
-          {/* Display notifications when not connecting or connected */}
-          {!isConnectingOrConnected && (
-            <div className="sm-max-w-xs sm-z-10">
-              <Notifications greeting={greeting} />
-            </div>
-          )}
-
-          {/* Display connect button when disconnected */}
-          {isDisconnected && (
-            <div className="sm-w-18 sm-h-18 md:sm-w-35 md:sm-h-35">
-              <ConnectButton>
-                <ProfileImage src={profilePicture} />
-              </ConnectButton>
-            </div>
-          )}
-
-          {/* Display progress indicator */}
-          <ProgressIndicatorWrapper transitionIn={isConnecting} position={position}>
-            <ProgressIndicator indicator={loadingIndicator} connectionState={connectionState} />
-          </ProgressIndicatorWrapper>
-
-          {/* Display video and controls if in FLOAT layout and connected */}
-          {layout === widgetLayout.FLOAT && isConnected && (
-            // <div className="sm-floating-container">
-            <div className="sm-w-full sm-h-full sm-round-shadow-box sm-border-2 sm-border-solid sm-border-gray-lightest">
-              <Video autoConnect={false} />
-              <div className="sm-absolute sm-top-0 sm-left-0 sm-w-full sm-h-full">
-                <VideoControls />
-              </div>
-            </div>
-            // </div>
-          )}
-        </div>
-      </div>
+    <div>
       <div className="sm-sticky sm-top-0 sm-w-full sm-h-full">
         <Video autoConnect={false} />
         <div className="sm-absolute sm-top-0 sm-left-0 sm-w-full sm-h-full">
@@ -112,32 +63,67 @@ export function Widget({
           </BackdropBlur>
         </div>
       </div>
-      {/* Modal for full-frame layout when connected */}
-      <Modal
-        title="Interactive Digital Person"
-        isOpen={isConnected && layout === widgetLayout.FULL_FRAME}
-        onClose={() => toggleLayout()}
-        panelRef={modalPanelRef}
-      >
-        {/* <div className="sm-sticky sm-top-0 sm-w-full sm-h-full">
-          <Video autoConnect={false} />
-          <div className="sm-absolute sm-top-0 sm-left-0 sm-w-full sm-h-full">
-            <BackdropBlur scrollTargetRef={modalPanelRef} smallScreenOnly={true}>
-              <VideoControls />
-            </BackdropBlur>
-          </div>
-        </div> */}
-
+      <div className="sm-fixed sm-text-primary-text sm-z-max sm-pointer-events-none sm-h-full sm-w-full sm-p-5">
         <div
-          className="sm-absolute sm-top-0 sm-left-0 sm-w-full sm-h-full
-            minRatio4/3:sm-w-auto minRatio4/3:sm-h-auto minRatio4/3:sm-left-auto
-            minRatio4/3:sm-top-24 minRatio4/3:sm-right-24 xl:sm-top-40 xl:sm-right-40 sm-pointer-events-none"
+          className={classNames('sm-h-full sm-flex sm-flex-col', {
+            'sm-gap-y-2 md:sm-gap-y-5 sm-justify-end':
+              layout === widgetLayout.FLOAT && position !== widgetPosition.TOP_LEFT,
+            'sm-justify-start': position === widgetPosition.TOP_LEFT,
+          })}
         >
-          <div className="sm-pt-[60vh] sm-flex sm-justify-center sm-pb-8 minRatio4/3:sm-pt-0">
-            <ContentCards fullHeight={true} />
+          <div
+            className={classNames('sm-flex sm-flex-wrap sm-gap-2 sm-items-center', {
+              'sm-justify-end sm-items-end': position === widgetPosition.BOTTOM_RIGHT,
+              'sm-justify-start sm-items-start':
+                position === widgetPosition.BOTTOM_LEFT || position === widgetPosition.TOP_LEFT,
+              'sm-flex-row-reverse': position === widgetPosition.BOTTOM_LEFT,
+            })}
+          >
+            {/* Display notifications when not connecting or connected */}
+            {!isConnectingOrConnected && (
+              <div className="sm-max-w-xs sm-z-10">
+                <Notifications greeting={greeting} />
+              </div>
+            )}
+
+            {/* Display connect button when disconnected */}
+            {isDisconnected && (
+              <div className="sm-w-18 sm-h-18 md:sm-w-35 md:sm-h-35">
+                <ConnectButton>
+                  <ProfileImage src={profilePicture} />
+                </ConnectButton>
+              </div>
+            )}
+
+            {/* Display progress indicator */}
+            <ProgressIndicatorWrapper transitionIn={isConnecting} position={position}>
+              <ProgressIndicator indicator={loadingIndicator} connectionState={connectionState} />
+            </ProgressIndicatorWrapper>
+
+            {/* Display video and controls if in FLOAT layout and connected */}
+            {layout === widgetLayout.FLOAT && isConnected && (
+              //  <div className="sm-floating-container">
+              <div className="sm-w-full sm-h-full sm-round-shadow-box sm-border-2 sm-border-solid sm-border-gray-lightest">
+                <Video autoConnect={false} />
+                <div className="sm-absolute sm-top-0 sm-left-0 sm-w-full sm-h-full">
+                  <VideoControls />
+                </div>
+              </div>
+              //  </div>
+            )}
           </div>
         </div>
-      </Modal>
+        {/* Khurram Shane hacking
+      <div className="sm-sticky sm-top-0 sm-w-full sm-h-full">
+        <Video autoConnect={false} />
+        <div className="sm-absolute sm-top-0 sm-left-0 sm-w-full sm-h-full">
+          <BackdropBlur scrollTargetRef={modalPanelRef} smallScreenOnly={true}>
+            <VideoControls />
+          </BackdropBlur>
+        </div>
+      </div> */}
+        {/* Modal for full-frame layout when connected */}
+      </div>
     </div>
   );
 }
